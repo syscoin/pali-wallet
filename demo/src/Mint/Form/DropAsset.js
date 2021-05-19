b8d39d63ff9841468d12bccdbb1da10fcb6fc73f
LucasBitt7 <lucas.s.bitencourt07@gmail.com>
2021-05-19 14:29:16 -0300

    asset dropdown with id and asset

---
+++ demo/src/Mint/Form/DropAsset.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+
+const Dropdown = () => {
+
+    const [assetGuid, setAssetGuid] = useState('');
+    const [receiver, setReceiver] = useState('');
+    const [rbf, setRbf] = useState(false);
+    const [amount, setAmount] = useState(0);
+    const [data, setData] = useState([]);
+    const [isInstalled, setIsInstalled] = useState(false);
+    const [canConnect, setCanConnect] = useState(true);
+    const [balance, setBalance] = useState(0);
+    const [controller, setController] = useState();
+    const [connectedAccount, setConnectedAccount] = useState({});
+    const [connectedAccountAddress, setConnectedAccountAddress] = useState("");
+  
+    useEffect(() => {
+      const callback = (event) => {
+        if (event.detail.SyscoinInstalled) {
+          setIsInstalled(true);
+  
+          if (event.detail.ConnectionsController) {
+            setController(window.ConnectionsController);
+  
+            return;
+          }
+  
+          return;
+        }
+  
+        setIsInstalled(false);
+  
+        window.removeEventListener("SyscoinStatus", callback);
+      }
+  
+      window.addEventListener("SyscoinStatus", callback);
+    }, []);
+  
+    const setup = async () => {
+      const state = await controller.getWalletState();
+  
+      if (state.accounts.length > 0) {
+        controller.getConnectedAccount()
+          .then((data) => {
+            if (data) {
+              setConnectedAccount(data);
+              setConnectedAccountAddress(data.address.main);
+              setBalance(data.balance);
+  
+              return;
+            }
+  
+            setConnectedAccount({});
+            setConnectedAccountAddress("");
+            setBalance(0);
+  
+            return;
+          });
+      }
+    };
+  
+    useEffect(() => {
+      if (controller) {
+        setup();
+  
+        controller.onWalletUpdate(setup);
+      }
+    }, [
+      controller,
+    ]);
+  
+    useEffect(() => {
+      console.log('tokens', data);
+  
+      const setup = async () => {
+        if (controller) {
+          console.log('torewkens');
+          setData(await controller.getUserMintedTokens());
+        }
+      }
+  
+      setup();
+    }, [
+      controller
+    ]);
+  
+    const RenderAsset = () => {
+       return data.map((asset, index) => {
+       return <option key={index}>Symbol:"{asset.symbol}"   Asset: "{asset.assetGuid}"</option>
+       });
+     }
+  
+    return (
+
+                    <div className="input-group mb-3">
+            <label htmlFor="assetGuid">AssetGuid:</label>
+
+            <select
+              id="assetGuid" 
+              name="assetGuid" 
+              className="custom-select"
+              onBlur={(event) => setAssetGuid(event.target.value)}
+            >
+              <option>{assetGuid}</option>
+              <RenderAsset />
+            </select>
+          </div>
+    );
+  }
+
+export default Dropdown;
--- demo/src/Mint/Form/FormMintSPT.js
+++ demo/src/Mint/Form/FormMintSPT.js
@@ -1,93 +1,12 @@
 import React, { useState, useEffect } from "react";
-
+import Dropdown from "./DropAsset"
 const FormMintSPT = (props) => {
   const [assetGuid, setAssetGuid] = useState('');
   const [receiver, setReceiver] = useState('');
   const [rbf, setRbf] = useState(false);
   const [amount, setAmount] = useState(0);
-  const [data, setData] = useState([]);
-  const [isInstalled, setIsInstalled] = useState(false);
-  const [canConnect, setCanConnect] = useState(true);
-  const [balance, setBalance] = useState(0);
-  const [controller, setController] = useState();
-  const [connectedAccount, setConnectedAccount] = useState({});
-  const [connectedAccountAddress, setConnectedAccountAddress] = useState("");
-
-  useEffect(() => {
-    const callback = (event) => {
-      if (event.detail.SyscoinInstalled) {
-        setIsInstalled(true);
-
-        if (event.detail.ConnectionsController) {
-          setController(window.ConnectionsController);
-
-          return;
-        }
-
-        return;
-      }
-
-      setIsInstalled(false);
-
-      window.removeEventListener("SyscoinStatus", callback);
-    }
-
-    window.addEventListener("SyscoinStatus", callback);
-  }, []);
-
-  const setup = async () => {
-    const state = await controller.getWalletState();
-
-    if (state.accounts.length > 0) {
-      controller.getConnectedAccount()
-        .then((data) => {
-          if (data) {
-            setConnectedAccount(data);
-            setConnectedAccountAddress(data.address.main);
-            setBalance(data.balance);
-
-            return;
-          }
-
-          setConnectedAccount({});
-          setConnectedAccountAddress("");
-          setBalance(0);
-
-          return;
-        });
-    }
-  };
-
-  useEffect(() => {
-    if (controller) {
-      setup();
-
-      controller.onWalletUpdate(setup);
-    }
-  }, [
-    controller,
-  ]);
-
-  useEffect(() => {
-    console.log('tokens', data);
-
-    const setup = async () => {
-      if (controller) {
-        setData(await controller.getUserMintedTokens());
-      }
-    }
-
-    setup();
-  }, [
-    controller
-  ]);
-
-  const RenderAsset = () => {
-    return data.map((asset, index) => {
-      return <option key={index}>{asset.assetGuid}</option>
-    });
-  }
-
+  
+ 
   return (
     <form
       onSubmit={(event) => props.formCallback(
@@ -103,19 +22,7 @@ const FormMintSPT = (props) => {
         <legend>YOU ARE MINTING SPTS</legend>
 
         <div>
-          <div className="input-group mb-3">
-            <label htmlFor="assetGuid">AssetGuid:</label>
-
-            <select
-              id="assetGuid" 
-              name="assetGuid" 
-              className="custom-select"
-              onBlur={(event) => setAssetGuid(event.target.value)}
-            >
-              <option>Choose...</option>
-              <RenderAsset />
-            </select>
-          </div>
+          <Dropdown/>
 
           <label htmlFor="amount">Amount:</label>
           <input 
