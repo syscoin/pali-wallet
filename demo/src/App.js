4dfe80d018430fcc20748ab3242b71c44da541ca
vieiraricardo <hi@ricvieira.com>
2021-06-09 23:21:34 -0300

    fix connected account route flow

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
-
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./routes/Home";
 import Dashboard from "./routes/Dashboard";
@@ -13,26 +10,42 @@ import IssueSPT from "./routes/IssueSPT";
 import Update from "./routes/Update";
 import Transfer from "./routes/Transfer";
 import About from "./routes/About";
-import Loader from "./routes/Loader";
 
 import Header from "./components/Header";
 import store from "./state/store";
+import setupState from "./utils/setupState";
 
 const App = () => {
   const [isLoading, setIsloading] = useState(true);
+  const [isConnected, setIsConnected] = useState(false);
+
+  window.onload = async function () {
+    const _isConnected = await setupState(store);
+
+    setIsConnected(_isConnected);
+    setIsloading(!isLoading);
+  };
 
   return (
     <div className="content">
       <BrowserRouter>
         <Provider store={store}>
           {isLoading ? (
-            <Loader loading={setIsloading} />
+            <></>
           ) : (
             <>
               <Header />
               <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/dashboard" exact component={Dashboard} />
+                <Route
+                  path="/"
+                  exact
+                  component={!isConnected ? Home : Dashboard}
+                />
+                {isConnected ? (
+                  <Route path="/dashboard" component={Dashboard} />
+                ) : (
+                  <Redirect to="/" />
+                )}
                 <Route path="/create-nft" component={CreateNFT} />
                 <Route path="/create-spt" component={CreateSPT} />
                 <Route path="/issue-spt" component={IssueSPT} />
--- demo/src/routes/Home.js
+++ demo/src/routes/Home.js
@@ -1,23 +1,27 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
 
-
-
+import store from "../state/store";
+import setupState from "../utils/setupState";
 
 // peace uncle grit essence stuff angle cruise annual fury letter snack globe
 
-const  Home = () => {
+export default function Home() {
   const accountData = useSelector((state) => state.connectedAccountData);
   const controller = useSelector((state) => state.controller);
-  const isInstalled = useSelector((state) => state.isInstalled);
+  const isConnected = useSelector((state) => state.connected);
+  const history = useHistory();
 
-  const handleMessageExtension = async () => {
-    controller
-      ? await controller.connectWallet()
-      : await window.ConnectionsController.connectWallet();
-  };
-  const truncate = (str) => {
-    return str.substr(0, 9) + "..." + str.substr(-5);
+  const handleConnect = async (event) => {
+    event.preventDefault(store);
+
+    if (controller && !isConnected) {
+      await controller.connectWallet();
+
+      await setupState(store);
+
+      history.push("/dashboard");
+    }
   };
 
   return (
@@ -38,7 +42,7 @@ const  Home = () => {
           velit quis orci rutrum congue ut euismod odio. Nunc non ipsum lacus.
           Pellentesque at urna sed arcu ultricies fringilla sit amet a purus.
         </p>
-        <form onSubmit={handleMessageExtension}>
+        <form onSubmit={handleConnect}>
           <select className="form-control">
             <option>1</option>
             <option>2</option>
@@ -46,19 +50,17 @@ const  Home = () => {
           </select>
 
           <div className="btn-center">
-          <button
+            <button
               title={accountData.connectedAccountAddress}
               className="button"
-              onClick={handleMessageExtension}
-              disabled={!isInstalled}>
-              {accountData.connectedAccountAddress === ""
-              ? "Connect to Syscoin Wallet"
-              : truncate(accountData.connectedAccountAddress)}
-          </button>
+              onClick={handleConnect}
+              disabled={!controller}
+            >
+              Connect to Syscoin Wallet
+            </button>
           </div>
         </form>
       </div>
     </section>
   );
 }
-export default Home
