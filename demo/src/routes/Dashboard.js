f7706d1ea22b84c593cba25491936c2ab99687e3
vieiraricardo <hi@ricvieira.com>
2021-06-11 11:36:35 -0300

    initial version of user assets rendering page

--- demo/src/routes/Dashboard.js
+++ demo/src/routes/Dashboard.js
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+
 import asset2 from "../images/asset2.svg";
 
 export default function Dashboard() {
-  const controller = useSelector((state) => state.controller);
-  const [mintedTokens, setMintedTokens] = useState([]);
+  const [assets, setAssets] = useState([]);
+  const { balance, connectedAccount } = useSelector(
+    (state) => state.connectedAccountData
+  );
 
   useEffect(() => {
-    (async () => {
-      const tokens = await controller.getUserMintedTokens();
-      setMintedTokens(tokens);
-    })();
-  }, []);
+    connectedAccount && setAssets(connectedAccount.assets);
+  }, [connectedAccount]);
+
+  const RenderBalance = ({ balance }) => {
+    const _balance = `${balance}`.split(".");
+    return (
+      <>
+        {Intl.NumberFormat("en-US").format(_balance[0])}
+        <span className="decimal">.{_balance[1]}</span> <em>SYS</em>
+      </>
+    );
+  };
 
   return (
     <section>
@@ -20,19 +30,20 @@ export default function Dashboard() {
         <div className="holdings bottom-border">
           <h2>Holdings</h2>
           <div className="sysvalue">
-            203,500<span className="decimal">.866544444</span> <em>SYS</em>
+            <RenderBalance balance={balance} />
           </div>
         </div>
         <div className="assets">
-          {Array.from({ length: 10 }).map((e, i) => (
-            <div className="asset" key={i}>
-              <embed src={asset2} />
+          {assets.map((asset) => (
+            <div className="asset" key={asset.assetGuid}>
+              <img src={asset2} />
               <div className="balance">
-                343,455<span className="decimal">.866544444</span>
+                {asset.balance}
+                {/*<span className="decimal">.866544444</span>*/}
               </div>
-              <div className="symbol">XYZ</div>
-              <div className="asset-id">Asset ID: 234234235235</div>
-              <div className="nft-id">NFT ID: 4324234234242</div>
+              <div className="symbol">{asset.symbol}</div>
+              <div className="asset-id">Asset ID: {asset.assetGuid}</div>
+              {/* <div className="nft-id">NFT ID: 4324234234242</div> */}
             </div>
           ))}
         </div>
