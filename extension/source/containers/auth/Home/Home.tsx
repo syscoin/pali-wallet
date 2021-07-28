2c1817a619f264f0934c5d92dbba5c0450f3a04b
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-07-28 17:48:53 -0300

    add gitignore

--- extension/source/containers/auth/Home/Home.tsx
+++ extension/source/containers/auth/Home/Home.tsx
@@ -241,6 +241,7 @@ const Home = () => {
                 value={String(activeAccountId)}
                 options={accounts}
                 onChange={(val: string) => {
+                  console.log('account selected', val)
                   controller.wallet.switchWallet(Number(val));
                   controller.wallet.account.watchMemPool(accounts[Number(val)]);
                 }}
