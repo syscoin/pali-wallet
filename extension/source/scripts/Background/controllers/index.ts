0f230ce963d22ff52fe32370a787af9e8a1a05be
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-08-11 15:21:08 -0300

    remove controller

--- extension/source/scripts/Background/controllers/index.ts
+++ extension/source/scripts/Background/controllers/index.ts
@@ -1,21 +1,18 @@
 import WalletController from './WalletController';
 import ControllerUtils from './ControllerUtils';
 import ConnectionsController from './ConnectionsController';
-import MessagesController from './MessagesController';
 
 export interface IMasterController {
   appRoute: (newRoute?: string) => string;
   connections: Readonly<IConnectionsController>;
   stateUpdater: () => void;
   wallet: Readonly<IWalletController>;
-  messages: Readonly<IMessagesController>
 }
 
 const MasterController = (): IMasterController => {
   const wallet = Object.freeze(WalletController());
   const utils = Object.freeze(ControllerUtils());
   const connections = Object.freeze(ConnectionsController());
-  const messages = Object.freeze(MessagesController());
 
   const stateUpdater = () => {
     utils.updateFiat();
@@ -26,7 +23,6 @@ const MasterController = (): IMasterController => {
     connections,
     appRoute: utils.appRoute,
     stateUpdater,
-    messages
   };
 };
 
