b62c5841e5a00f1b98bdec461f703142be1d7cfe
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-20 13:24:46 -0300

    update pali documentation link

--- extension/source/containers/auth/Settings/views/AboutView/index.tsx
+++ extension/source/containers/auth/Settings/views/AboutView/index.tsx
@@ -8,16 +8,16 @@ const AboutView: FC = () => {
   const alert = useAlert();
   const handleSupportClick = () => {
     alert.show('You will be redirected to Syscoin Discord, please contact support team at #pali_support', {
-      timeout: 6000, // custom timeout just for this one alert
+      timeout: 5000,
       type: 'success',
       onClose: () => {
         window.open('https://discord.gg/6rKsX9SqUr')
-      } // callback that will be executed after this alert is removed
+      }
     });
 
   };
   const handleDocsClick = () => {
-    window.open('https://pali-docs.vercel.app/');
+    window.open('https://docs.paliwallet.com/');
   };
   return (
     <div className={styles.about}>
@@ -27,22 +27,18 @@ const AboutView: FC = () => {
         Support:{' '}
         <a
           className={styles.link}
-          // href="#"
-          // target="_blank"
           onClick={handleSupportClick}
         >
-          pali support
+          Pali support
         </a>
       </span>
       <span>
         API Docs
         <a
           className={clsx(styles.link, styles.terms)}
-          // href="#"
-          // target="_blank"
           onClick={handleDocsClick}
         >
-          pali API
+          Pali API
         </a>
       </span>
     </div>
