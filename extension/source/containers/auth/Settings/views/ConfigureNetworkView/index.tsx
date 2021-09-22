4258719f0aeb005b7ec90155536606b539ddcb66
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-22 17:48:36 -0300

    update network config component

--- extension/source/containers/auth/Settings/views/ConfigureNetworkView/index.tsx
+++ extension/source/containers/auth/Settings/views/ConfigureNetworkView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -11,37 +11,105 @@ import Spinner from '@material-ui/core/CircularProgress';
 import { MAIN_VIEW } from '../routes';
 
 import styles from './index.scss';
+import { SYS_NETWORK } from 'constants/index';
 
 const ConfigureNetworkView = () => {
-  const [address, setAddress] = useState<string | undefined>();
   const controller = useController();
-  // const { handleSubmit, register } = useForm({
-  //   validationSchema: yup.object().shape({
-  //     name: yup.string().required(),
-  //   }),
-  // });
-  const [isCopied, copyText] = useCopyClipboard();
-  const [loading, setLoading] = useState<boolean>(false);
-  const showView = useSettingsView();
+  const [selected, setSelected] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const addressClass = clsx(styles.address, {
-    [styles.copied]: isCopied && address,
+  const { handleSubmit, register } = useForm({
+    validationSchema: yup.object().shape({
+      name: yup.string().required(),
+      blockbookURL: yup.string().required(),
+    }),
   });
 
   const onSubmit = async (data: any) => {
     setLoading(true);
-    const res = await controller.wallet.account.addNewAccount(data.name);
-
-    if (res) {
-      setAddress(res);
-      setLoading(false);
-
-      await controller.wallet.account.updateTokensState();
-    }
+    console.log('data on submit network', data, data.name, data.blockbookURL)
+    setSelected('');
   };
 
   return (
-    <div className={styles.configureNetworkWrapper}>
+    <div>
+      {selected ? (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.configureNetworkWrapper}>
+          <div className={styles.column}>
+            <TextInput
+              type="text"
+              name="name"
+              defaultValue={SYS_NETWORK[selected].label}
+              fullWidth
+              variant={styles.input}
+              placeholder="Network name"
+              inputRef={register}
+            />
+
+            <TextInput
+              type="text"
+              name="blockbookURL"
+              defaultValue={SYS_NETWORK[selected].beUrl}
+              fullWidth
+              variant={styles.input}
+              placeholder="Blockbook URL"
+              inputRef={register}
+            />
+          </div>
+
+          <div className={styles.actions}>
+            <Button
+              type="button"
+              theme="btn-outline-secondary"
+              variant={clsx(styles.button, styles.close)}
+              onClick={() => setSelected('')}
+            >
+              Close
+            </Button>
+
+            <Button
+              type="submit"
+              theme="btn-outline-primary"
+              variant={styles.button}
+            >
+              Confirm
+            </Button>
+          </div>
+        </form>
+      ) : (
+        <div className={styles.configureNetworkWrapper}>
+          <ul className={styles.networksList}>
+            {Object.values(SYS_NETWORK).map((network: any) => {
+              return (
+                <li
+                  key={network.id}
+                  className={styles.networkItem}
+                  onClick={() => {
+                    setSelected(network.id);
+                  }}
+                >
+                  <span>
+                    {network.label}
+                  </span>
+
+                  <small>
+                    Blockbook URL:
+                    <span> {network.beUrl}</span>
+                  </small>
+                </li>
+              )
+            })}
+          </ul>
+
+          <Button
+            type="button"
+            theme="btn-gradient-primary"
+            variant={styles.button}
+          >
+            Configure new network
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
