e9e81c0cb2999240f8a119ca46731f7ec93f705d
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-11-18 16:01:19 -0300

    feat: add hooks

--- extension/source/pages/App/App.tsx
+++ extension/source/pages/App/App.tsx
@@ -1,16 +1,12 @@
 import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Container from 'containers/common/Container/index';
 import AuthRouter from 'routers/Auth';
 import UnAuthRouter from 'routers/UnAuth';
-import { RootState } from 'state/store';
-import IWalletState from 'state/wallet/types';
+import { useStore } from 'hooks/index';
 
 const App: FC = () => {
-  const { encriptedMnemonic }: IWalletState = useSelector(
-    (state: RootState) => state.wallet
-  );
+  const { encriptedMnemonic } = useStore();
 
   return (
     <section className="min-w-popup">
