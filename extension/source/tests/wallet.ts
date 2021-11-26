b249c58168fffff3d455350d249530210aa493ec
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-11-26 17:48:55 -0300

    chore: wip

---
+++ extension/source/tests/wallet.ts
@@ -0,0 +1,196 @@
+export const wallet = [
+  {
+    description: 'controller.wallet.setWalletPassword(data.password)',
+  },
+  {
+    description: 'controller.wallet.generatePhrase()'
+  },
+  {
+    description: 'controller.wallet.importPhrase(phrase)'
+  },
+  {
+    description: 'controller.wallet.createWallet()'
+  },
+  {
+    description: 'controller.wallet.isLocked()'
+  },
+  {
+    description: 'controller.wallet.getNewAddress()'
+  },
+  {
+    description: 'controller.wallet.switchWallet(Number(val))'
+  },
+  {
+    description: 'controller.wallet.createHardwareWallet()'
+  },
+  {
+    description: 'controller.wallet.checkPassword(password)'
+  },
+  {
+    description: 'controller.wallet.deleteWallet(password)'
+  },
+  {
+    description: 'controller.wallet.logOut()'
+  },
+  {
+    description: 'controller.wallet.getPhrase(password)'
+  },
+  {
+    description: 'controller.wallet.unLock(password)'
+  },
+]
+
+export const account = [
+  {
+    description: 'controller.wallet.accountgetTransactionInfoByTxId(txid)',
+  },
+  {
+    description: 'controller.wallet.account.getSysExplorerSearch()'
+  },
+  {
+    description: 'controller.wallet.account.getLatestUpdate()'
+  },
+  {
+    description: 'controller.wallet.account.watchMemPool(id)'
+  },
+  {
+    description: 'controller.wallet.account.getTransactionData(txid)'
+  },
+  {
+    description: 'controller.wallet.account.getDataAsset(assetguid)'
+  },
+  {
+    description: 'controller.wallet.account.updateTxs()'
+  },
+  {
+    description: 'controller.wallet.account.getTransactionItem()'
+  },
+  {
+    description: 'controller.wallet.account.getRecommendFee()'
+  },
+  {
+    description: 'controller.wallet.account.confirmTempTx()'
+  },
+  {
+    description: 'controller.wallet.account.updateTempTx()'
+  },
+  {
+    description: 'controller.wallet.account.isValidSYSAddress()'
+  },
+  {
+    description: 'controller.wallet.account.isNFT()'
+  },
+  {
+    description: 'controller.wallet.account.setAutolockTimer()'
+  },
+  {
+    description: 'controller.wallet.account.addNewAccount()'
+  },
+  {
+    description: 'controller.wallet.account.updateTokensState()'
+  },
+  {
+    description: 'controller.wallet.account.decryptAES()'
+  },
+  {
+    description: 'controller.wallet.account.confirmNewSPT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromWalletToCreateSPT()'
+  },
+  {
+    description: 'controller.wallet.account.confirmIssueSPT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromWalletToMintSPT()'
+  },
+  {
+    description: 'controller.wallet.account.confirmIssueNFT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromWalletToMintNFT()'
+  },
+  {
+    description: 'controller.wallet.account.confirmIssueNFTTx()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromWalletToIssueNFT()'
+  },
+  {
+    description: 'controller.wallet.account.importPsbt()'
+  },
+  {
+    description: 'controller.wallet.account.getDataAsset()'
+  },
+  {
+    description: 'controller.wallet.account.confirmTransferOwnership()'
+  },
+  {
+    description: 'controller.wallet.account.confirmUpdateAssetTransaction()'
+  },
+  {
+    description: 'controller.wallet.account.confirmUpdateAssetTransaction()'
+  },
+  {
+    description: 'controller.wallet.account.getTransactionInfoByTxId()'
+  },
+  {
+    description: 'controller.wallet.account.setHDSigner()'
+  },
+  {
+    description: 'controller.wallet.account.getConnectedAccountXpub()'
+  },
+  {
+    description: 'controller.wallet.account.getChangeAddress()'
+  },
+  {
+    description: 'controller.wallet.account.isValidSYSAddress()'
+  },
+  {
+    description: 'controller.wallet.account.getHoldingsData()'
+  },
+  {
+    description: 'controller.wallet.account.createSPT()'
+  },
+  {
+    description: 'controller.wallet.account.getDataFromPageToInitTransaction()'
+  },
+  {
+    description: 'controller.wallet.account.getUserMintedTokens()'
+  },
+  {
+    description: 'controller.wallet.account.setCurrentPSBT()'
+  },
+  {
+    description: 'controller.wallet.account.setCurrentPsbtToSign()'
+  },
+  {
+    description: 'controller.wallet.account.updateTempTx()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToCreateNewSPT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToMintSPT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToUpdateAsset()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToTransferOwnership()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToIssueNFT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToMintNFT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToMintNFT()'
+  },
+  {
+    description: 'controller.wallet.account.setDataFromPageToMintNFT()'
+  },
+]
+
+export const connection = [];
