4d5206135fa357e9e2cca773410cca9060f7dcb2
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-02 16:31:15 -0300

    fix receiving address issue

--- extension/source/scripts/Background/controllers/WalletController.ts
+++ extension/source/scripts/Background/controllers/WalletController.ts
@@ -41,10 +41,10 @@ const WalletController = (): IWalletController => {
   };
 
   const checkPassword = (pwd: string) => {
-    if(encriptedPassword === CryptoJS.SHA3(pwd).toString()) {
+    if (encriptedPassword === CryptoJS.SHA3(pwd).toString()) {
       return true;
     }
-    
+
     return encriptedPassword === pwd;
   };
 
@@ -295,6 +295,8 @@ const WalletController = (): IWalletController => {
 
       store.dispatch(updateSwitchNetwork(true));
 
+      console.log('new signer main', sjs, HDsigner)
+
       _getAccountDataByNetwork(sjs);
 
       return;
@@ -303,6 +305,7 @@ const WalletController = (): IWalletController => {
     setSjs({ SignerIn: HDsigner, blockbookURL: SYS_NETWORK.testnet.beUrl });
 
     store.dispatch(updateSwitchNetwork(true));
+    console.log('new signer testnet', sjs, HDsigner)
 
     _getAccountDataByNetwork(sjs);
   };
@@ -346,7 +349,11 @@ const WalletController = (): IWalletController => {
       address = account0.getAddress(receivingIndex + 1);
     } else {
       try {
+        sjs.Signer.Signer.receivingIndex = -1;
+
         address = await sjs.Signer.getNewReceivingAddress();
+
+        console.log('new address from sjs', address);
       } catch (error: any) {
         console.log('error getting receiving address from sysjs', error);
 
