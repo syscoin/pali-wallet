bd90d1474ba92e7be094b78f7f0a0f7ff7058d14
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-18 18:40:23 -0300

    chore: wip

--- extension/source/containers/auth/Settings/views/ConnectHardwareWalletView/index.tsx
+++ extension/source/containers/auth/Settings/views/ConnectHardwareWalletView/index.tsx
@@ -4,81 +4,89 @@ import { AuthViewLayout } from 'containers/common/Layout';
 
 import { Form, Input } from 'antd';
 import { WarningCard } from 'components/Cards';
+import { useUtils } from 'hooks/useUtils';
+import { Disclosure } from '@headlessui/react';
 
 const ConnectHardwareWalletView: FC = () => {
   const [selected, setSelected] = useState<boolean>(false);
 
+  const { history } = useUtils();
+
   return (
-    <>
-      <AuthViewLayout title="CONNECT HARDWARE WALLET"> </AuthViewLayout>
-      <Form
-          className="flex justify-center items-center flex-col gap-2 text-center pt-4"
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          autoComplete="off"
-        >
-        <div>
-          <p className="text-justify text-base text-brand-white px-7">Select a hardware wallet you'd like to use with Pali Wallet</p>
-        </div>
-            <Form.Item
-                name="password"
-            >
-                <Input className="h-12" />
-            </Form.Item>
-        </Form>
-        <div>
-          <WarningCard
-            className="w-full rounded text-brand-blueWarningTest border-dashed border border-brand-blueWarningTest text-justify "
-            warningText="Don't have a hardware wallet?"
+    <AuthViewLayout title="CONNECT HARDWARE WALLET">
+      <div className="flex items-center flex-col justify-center w-full">
+        <div className="text-sm overflow-auto px-4 h-96">
+          <p className="text-white text-sm py-3 pl-1 mt-3">
+            Select the hardware wallet you'd like to connect to Pali
+          </p>
+
+          <p
+            className="rounded-full py-3 px-4 w-44 mx-auto text-center bg-brand-navyborder border border-brand-royalBlue text-sm mb-6 mt-3"
           >
-            <br />
-            <p className="pt-4">Order a Trezor wallet and keep your founds  
-            in cold storage.
+            Trezor
+          </p>
+
+          <div className="bg-brand-navydarker border border-dashed border-brand-royalBlue text-brand-white mx-2 p-4 text-xs rounded-lg">
+            <p>
+              <b>Don't have a hardware wallet?</b><br />
+
+              Order a Trezor wallet and keep your funds in cold storage.
             </p>
-            <br />
-            <a className="text-brand-white" href="https://trezor.io/" target="_blank" rel="noreferrer">
+
+            <p onClick={() => window.open('https://trezor.io/')}>
               Buy now
-            </a>
-          </WarningCard>
+            </p>
+          </div>
+
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <Disclosure.Button>
+                  <Disclosure.Button
+                    className="my-3 py-2 px-4 flex justify-between items-center rounded-lg w-full border border-brand-royalBlue cursor-pointer transition-all duration-300 bg-brand-navydarker"
+                  >
+                    Learn more
+
+                    <Icon
+                      name="select-up"
+                      className={`${open ?
+                        'transform rotate-180' :
+                        ''
+                        } mb-1 text-brand-deepPink100`}
+                    />
+
+                  </Disclosure.Button>
+                </Disclosure.Button>
+
+                <Disclosure.Panel>
+                  <div className="mx-2 my-6 py-2 px-4 flex justify-between items-center rounded-lg w-80 border border-brand-royalBlue cursor-pointer transition-all duration-300 bg-brand-navydarker">
+                    <p>
+                      1 - Connect a hardware wallet
+                      Connect your hardware wallet directly to your computer.
+
+                      2 - Start using sys powered sites and more!
+                      Use your hardware account like you would with any SYS account. Connect to SYS web3 sites, send SYS, buy and store SPT tokens.
+
+                    </p>
+                  </div>
+
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
         </div>
-        <div className="p-7">
-          <button className="inline-flex text-sm text-brand-white w-full text-justify bg-brand-navydarker px-4 py-2" onClick={() => setSelected(!selected)}>Learn more
-          {selected ? (
-            <Icon name="up" className="inline-flex self-center text-sm pl-48" maxWidth={"1"}></Icon>
-          ) : (
-            <Icon name="down" className="inline-flex self-center text-sm pl-48" maxWidth={"1"}></Icon>
-          )}
-            
-          </button>
-          {selected ? (
-            <div className="text-brand-white text-sm bg-brand-navylight px-4 py-2">
-              <p>
-                <b>1 - Connect a hardware wallet</b>
-                <br />
-                Connect your hardware wallet directly to your computer.
-              </p>
-              <p className="pt-4">
-                <b>2 - Start using sys powered sites and more!</b>
-                <br />
-                Use your hardware account like you would with any SYS account. Connect to SYS web3 sites, send SYS, buy and store SPT tokens. 
-              </p>
-            </div>
-          ) : (
-            <div>
-            </div>
-          )}
-        </div>
-        
-        <div className="flex justify-center items-center pt-2">
-          <Button
-            type="submit"
-          >
-              Connect
-          </Button>
-        </div>
-    </>
+
+        <Button
+          type="button"
+          className="bg-brand-navydarker"
+          classNameBorder="absolute bottom-12"
+          onClick={() => history.push('/home')}
+          disabled={!selected}
+        >
+          Connect
+        </Button>
+      </div>
+    </AuthViewLayout>
   );
 };
 
