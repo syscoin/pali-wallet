47dcf9116d45853cb439e8cb8db4a7d6c4f413d7
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-22 17:17:58 -0300

    chore: wip reskin

--- extension/source/containers/auth/Settings/views/DeleteWalletView/DeleteWallet.tsx
+++ extension/source/containers/auth/Settings/views/DeleteWalletView/DeleteWallet.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { SecondaryButton, PrimaryButton } from 'components/index';
-import {
-  useUtils,
-} from 'hooks/index';
 import { AuthViewLayout } from 'containers/common/Layout';
 import { Form, Input } from 'antd';
-import { useController } from 'hooks/index';
+import { useController, useAccount, useUtils } from 'hooks/index';
 
 const DeleteWalletView = () => {
-  const {
-    history,
-  } = useUtils();
+  const { history } = useUtils();
+  const { activeAccount } = useAccount();
 
   const controller = useController();
 
@@ -24,6 +20,17 @@ const DeleteWalletView = () => {
     }
   };
 
+  const [form] = Form.useForm();
+
+  const password = form.getFieldValue('password');
+  const seed = form.getFieldValue('seed');
+  const walletSeedPhrase = controller.wallet.getPhrase(password);
+
+  const canDeleteWallet = !password ||
+    !seed ||
+    !walletSeedPhrase ||
+    walletSeedPhrase !== seed;
+
   return (
     <AuthViewLayout title="DELETE WALLET">
       <p className="text-white text-sm py-3 px-10 mt-8">
@@ -32,9 +39,10 @@ const DeleteWalletView = () => {
 
       <div className="flex justify-center items-center flex-col">
         <Form
+          form={form}
           onFinish={onSubmit}
           className="flex justify-center items-center flex-col gap-8 text-center pt-4 mb-12"
-          name="phraseview"
+          name="delete"
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
           autoComplete="off"
@@ -61,45 +69,49 @@ const DeleteWalletView = () => {
             ]}
           >
             <Input.Password
-              className="phrase-input rounded-full py-3 px-4 w-72 bg-brand-navyborder border border-brand-royalBlue text-sm outline-none"
+              className="phrase-input rounded-full py-2 px-4 bg-brand-navyborder border border-brand-royalBlue text-sm outline-none"
               placeholder="Enter your password"
             />
           </Form.Item>
 
-          <p className="bg-brand-navydark border border-dashed border-brand-deepPink100 mx-6 p-4 text-xs rounded-lg">
-            <b>WARNING:</b> You still have funds in your wallet. Paste your seed phrase below to delete wallet.
-          </p>
+          {activeAccount && activeAccount.balance > 0 && (
+            <>
+              <p className="bg-brand-navydark border border-dashed border-brand-deepPink100 mx-6 p-4 text-xs rounded-lg">
+                <b>WARNING:</b> You still have funds in your wallet. Paste your seed phrase below to delete wallet.
+              </p>
 
-          <div
-            className="flex flex-col justify-center items-center gap-3 bg-brand-navydarker border border-dashed border-brand-royalBlue mx-6 my-8 p-4 text-xs rounded-lg"
-          >
-            <Form.Item
-              name="seed"
-              dependencies={['password']}
-              rules={[
-                {
-                  required: true,
-                  message: '',
-                },
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    const seed = controller.wallet.getPhrase(getFieldValue('password'));
+              <div
+                className="flex flex-col justify-center items-center gap-3 bg-brand-navydarker border border-dashed border-brand-royalBlue mx-6 my-8 p-2 text-xs w-72 rounded-lg"
+              >
+                <Form.Item
+                  name="seed"
+                  dependencies={['password']}
+                  rules={[
+                    {
+                      required: true,
+                      message: '',
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        const seed = controller.wallet.getPhrase(getFieldValue('password'));
 
-                    if (seed === value) {
-                      return Promise.resolve();
-                    }
+                        if (seed === value) {
+                          return Promise.resolve();
+                        }
 
-                    return Promise.reject('');
-                  },
-                }),
-              ]}
-            >
-              <Input
-                className="bg-brand-navydarker outline-none w-72"
-                placeholder="Your seed phrase"
-              />
-            </Form.Item>
-          </div>
+                        return Promise.reject('');
+                      },
+                    }),
+                  ]}
+                >
+                  <Input
+                    className="bg-brand-navydarker border border-brand-navydarker text-sm w-60 outline-none py-2"
+                    placeholder="Your seed phrase"
+                  />
+                </Form.Item>
+              </div>
+            </>
+          )}
 
           <div className="absolute bottom-12 flex justify-between gap-x-4">
             <SecondaryButton
@@ -111,13 +123,12 @@ const DeleteWalletView = () => {
 
             <PrimaryButton
               type="submit"
+              disabled={canDeleteWallet}
             >
               Delete
             </PrimaryButton>
           </div>
         </Form>
-
-
       </div>
     </AuthViewLayout>
   );
