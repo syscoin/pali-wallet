dc9a0ad94e36c528a35f692e5d182819501cf046
Amanda Gonsalves <amandagonsalves75@gmail.com>
2022-01-14 13:01:09 -0300

    chore: add controller utils test

---
+++ source/scripts/Background/controllers/utils.spec.ts
@@ -0,0 +1,42 @@
+import { isNFT, countDecimals, sortList } from './utils';
+
+describe('controllers utils', () => {
+  it('should check if given token is a nft', () => {
+    expect(isNFT(831140473)).toEqual(false);
+  });
+
+  it('should check amount decimals to be less than or equal permitted decimals', () => {
+    const permittedDecimals = 8;
+    const valueDecimals = countDecimals(10.12345678);
+
+    expect(valueDecimals).toBeLessThanOrEqual(permittedDecimals);
+  });
+
+  it('should sort an alfabetic given list', () => {
+    const listToBeSorted = [
+      {
+        symbol: 'tsdb',
+      },
+      {
+        symbol: 'abcd',
+      },
+      {
+        symbol: 'dcdd',
+      },
+    ];
+
+    const sortedList = sortList(listToBeSorted);
+
+    expect(sortedList).toEqual([
+      {
+        symbol: 'abcd',
+      },
+      {
+        symbol: 'dcdd',
+      },
+      {
+        symbol: 'tsdb',
+      },
+    ]);
+  });
+});
--- source/web/__test__/accountController.test.js
+++ source/web/__test__/accountController.test.js
@@ -210,6 +210,92 @@ const getLatestUpdate = async () => {
   store.dispatch(updateSwitchNetwork(false));
 };
 
+const setNewAddress = (addr) => {
+  const { activeAccountId } = store.getState().wallet;
+
+  store.dispatch(
+    updateAccountAddress({
+      id: activeAccountId,
+      address: { main: addr },
+    })
+  );
+
+  return true;
+};
+
+const getHoldingsData = async () => {
+  const { walletTokens } = store.getState().wallet;
+
+  if (walletTokens) {
+    const connectedAccountId = walletTokens.findIndex(
+      (accountTokens) => accountTokens.accountId === getConnectedAccount().id
+    );
+
+    if (connectedAccountId > -1) {
+      return walletTokens[connectedAccountId].holdings;
+    }
+  }
+
+  return [];
+};
+
+const setAutolockTimer = (minutes) => {
+  store.dispatch(setTimer(minutes));
+};
+
+const updateNetworkData = ({ id, label, beUrl }) => {
+  store.dispatch(
+    updateNetwork({
+      id,
+      label,
+      beUrl,
+    })
+  );
+};
+
+const getConnectedAccount = () => {
+  const { accounts, tabs } = store.getState().wallet;
+  const { currentURL } = tabs;
+  return accounts.find((account) => {
+    return account.connectedTo.find((url) => {
+      return url === new URL(currentURL).host;
+    });
+  });
+};
+
+const coventPendingType = (txid) => ({
+  txid,
+  value: 0,
+  confirmations: 0,
+  fees: 0,
+  blockTime: Date.now() / 1e3,
+});
+
+const updateTransactionData = (txinfo) => {
+  let globalAccount;
+  const transactionItem =
+    store.getState().wallet.temporaryTransactionState.type === 'sendAsset';
+
+  let transactions = [];
+
+  if (transactionItem && globalAccount) {
+    transactions = globalAccount?.transactions;
+  }
+
+  if (!transactionItem && getConnectedAccount()) {
+    transactions = getConnectedAccount().transactions;
+  }
+
+  store.dispatch(
+    updateTransactions({
+      id: transactionItem
+        ? Number(globalAccount?.id)
+        : Number(getConnectedAccount().id),
+      txs: [coventPendingType(txinfo), ...transactions],
+    })
+  );
+};
+
 describe('Account Test', () => {
   it('should return a decrypt string', () => {
     const value = 'test';
@@ -217,19 +303,37 @@ describe('Account Test', () => {
     const decrypt = decryptAES(encrypt.toString(), 'test123');
     expect(decrypt).toBe(value);
   });
-  // it('should return a sys address verification', () => {
-  //   const invalidSysAddress = 'sys213ixks1mx';
-  //   const value = isValidSYSAddress(invalidSysAddress, 'main');
-  //   expect(value).toBeFalsy();
-  // });
-  // it('should return a transaction information', async () => {
-  //   const result = await getTransactionInfoByTxId(
-  //     initialMockState.accounts[0].transactions[0].txid
-  //   );
-  //   console.log(result);
-  // });
-  it('should return lastest update of active account', async () => {
-    const result = await getLatestUpdate();
-    console.log(result);
+  it('should return a sys address verification', () => {
+    const invalidSysAddress = 'sys213ixks1mx';
+    const value = isValidSYSAddress(invalidSysAddress, 'main');
+    expect(value).toBeFalsy();
+  });
+  it('should set a new address account', () => {
+    const newAddress = 'testAddress';
+    setNewAddress(newAddress);
+    const { accounts } = store.getState().wallet;
+    expect(accounts[0]?.address.main).toBe(newAddress);
+  });
+  it('should return holdings data', async () => {
+    const result = await getHoldingsData();
+    expect(result).toStrictEqual([]);
+  });
+  it('should set new autolock timer', () => {
+    const newTime = 10;
+    setAutolockTimer(newTime);
+    const { timer } = store.getState().wallet;
+    expect(timer).toBe(newTime);
+  });
+  it('should update networks info', () => {
+    const newLabel = 'test';
+    const newUrl = 'test.com';
+    updateNetworkData({ id: 'main', label: newLabel, beUrl: newUrl });
+    const { networks } = store.getState().wallet;
+    expect(networks.main.label).toBe(newLabel);
+  });
+  it('should update transaction data', () => {
+    const txId =
+      '89f20ae3ba21792b60dc32007b273dde4ffa7b9c389bbb688772974fbeb38962';
+    updateTransactionData(txId);
   });
 });
