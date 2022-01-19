d598f43ea0d0439635b246f10a15874a4e031e14
Olavo Ançay <63649496+olavoancay2802@users.noreply.github.com>
2022-01-19 11:40:05 -0300

    Adding some btn id's and classes

--- source/web/e2e/tests/CreateNewAccount.spec.js
+++ source/web/e2e/tests/CreateNewAccount.spec.js
@@ -1,21 +1,35 @@
-/*const initializator = require('../initializator');
+const initializator = require('../initializator');
 const { browser } = require('webextension-polyfill-ts');
 const { By } = require('selenium-webdriver');
+const { buildWebDriver } = require('../webdriver');
 
 describe('Create New Account', () => {
   it('should create new account after login', async () => {
+    let driver;
+    const { driver: webDriver } = await buildWebDriver();
+    driver = webDriver;
     await initializator();
-    await driver.clickElement('#kebab-menu-btn');
+
+    await driver.clickElement('.kebab-menu-btn');
+
     await driver.clickElement('.accounts-btn');
+
     await driver.clickElement('.create-account-btn');
+
     await driver.fill('.new-account-name-input', 'Test Account');
+
     await driver.clickElement('#create-btn');
+
     await driver.clickElement('#got-it-btn');
-    await driver.clickElement('#kebab-menu-btn');
+
+    await driver.clickElement('.kebab-menu-btn');
+
     await driver.clickElement('.accounts-btn');
+
     const findNewAccount = await driver.findElement(
       By.xpath("//*[text()='Test Account']")
     );
+
     if (findNewAccount) {
       console.log('New Account has been created');
     } else {
@@ -23,4 +37,4 @@ describe('Create New Account', () => {
     }
     driver.quit();
   });
-});*/
+});
--- source/web/e2e/tests/switchWallet.spec.js
+++ source/web/e2e/tests/switchWallet.spec.js
@@ -1,10 +1,10 @@
-const initializator = require('../initializator');
-const { browser } = require('webextension-polyfill-ts');
-const { By } = require('selenium-webdriver');
-const { buildWebDriver } = require('../webdriver');
+/*const initializator = require("../initializator");
+const { browser } = require("webextension-polyfill-ts");
+const { By } = require("selenium-webdriver");
+const { buildWebDriver } = require("../webdriver");
 
-describe('Switch Account', () => {
-  it('should switch account after create new account', async () => {
+describe("Switch Account", () => {
+  it("should switch account after create new account", async () => {
     let driver;
     const { driver: webDriver } = await buildWebDriver();
     driver = webDriver;
@@ -13,29 +13,29 @@ describe('Switch Account', () => {
       await driver.findElement(By.xpath("//*[text()='Account 1']"));
     }, 2000);
     if (findDefaultActiveAccount) {
-      console.log('Account 1 is the active account set by default');
+      console.log("Account 1 is the active account set by default");
     } else {
-      console.log('Account 1 is NOT the active account set by default');
+      console.log("Account 1 is NOT the active account set by default");
     }
-
-    await driver.clickElement('#kebab-menu-btn');
-
-    await driver.clickElement('.accounts-btn');
-    await driver.clickElement('.create-account-btn');
-    await driver.fill('.new-account-name-input', 'Test Account');
-    await driver.clickElement('#create-btn');
-    await driver.clickElement('#got-it-btn');
-    await driver.clickElement('#kebab-menu-btn');
-    await driver.clickElement('.accounts-btn');
-    await driver.clickElement('#account-1');
+    setTimeout(async () => {
+      await driver.clickElement("#kebab-menu-btn");
+    }, 2000);
+    await driver.clickElement(".accounts-btn");
+    await driver.clickElement(".create-account-btn");
+    await driver.fill(".new-account-name-input", "Test Account");
+    await driver.clickElement("#create-btn");
+    await driver.clickElement("#got-it-btn");
+    await driver.clickElement("#kebab-menu-btn");
+    await driver.clickElement(".accounts-btn");
+    await driver.clickElement("#account-1");
     const findNewActiveAccount = await driver.findElement(
       By.xpath("//*[text()='Test Account']")
     );
     if (findNewActiveAccount) {
-      console.log('Switch Account is working correctly');
+      console.log("Switch Account is working correctly");
     } else {
-      console.log('Switch Account is NOT working correctly');
+      console.log("Switch Account is NOT working correctly");
     }
     driver.quit();
   });
-});
+});*/
