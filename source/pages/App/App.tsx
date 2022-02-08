c944a5df8264476666851ce03ba42e5a09375e85
Lucas Gauer <lucasfgauer97@gmail.com>
2022-02-08 12:21:49 -0300

    fix: Add BrowserRouter

--- source/pages/App/App.tsx
+++ source/pages/App/App.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import Container from 'containers/common/Container';
 import { AuthRouter } from 'routers/index';
+import { BrowserRouter } from 'react-router-dom';
 
 const App: FC = () => (
   <section className="mx-auto min-w-popup h-full min-h-popup bg-bkg-2 md:max-w-2xl">
     <Container>
-      <AuthRouter />
+      <BrowserRouter>
+        <AuthRouter />
+      </BrowserRouter>
     </Container>
   </section>
 );
