5c4dbc307145481887534dd31a6c20136e591305
Olavo Ançay <63649496+olavoancay2802@users.noreply.github.com>
2022-02-24 16:25:18 -0300

    hot fix: add warning message on about screen

--- extension/source/containers/auth/Settings/views/AboutView/index.tsx
+++ extension/source/containers/auth/Settings/views/AboutView/index.tsx
@@ -1,20 +1,25 @@
 import React, { FC } from 'react';
 import clsx from 'clsx';
 import { useAlert } from 'react-alert';
-
+import IconButton from '@material-ui/core/IconButton';
+import CopyIcon from '@material-ui/icons/FileCopy';
 import styles from './index.scss';
+import { useCopyClipboard } from 'hooks/index';
 
 const AboutView: FC = () => {
+  const [isCopied, copyText] = useCopyClipboard();
   const alert = useAlert();
   const handleSupportClick = () => {
-    alert.show('You will be redirected to Syscoin Discord, please contact support team at #pali_support', {
-      timeout: 5000,
-      type: 'success',
-      onClose: () => {
-        window.open('https://discord.gg/8QKeyurHRd')
+    alert.show(
+      'You will be redirected to Syscoin Discord, please contact support team at #pali_support',
+      {
+        timeout: 5000,
+        type: 'success',
+        onClose: () => {
+          window.open('https://discord.gg/8QKeyurHRd');
+        },
       }
-    });
-
+    );
   };
   const handleDocsClick = () => {
     window.open('https://docs.paliwallet.com/');
@@ -25,12 +30,23 @@ const AboutView: FC = () => {
       <span>Version: 1.0.22</span>
       <span>
         Support:{' '}
-        <a
-          className={styles.link}
-          onClick={handleSupportClick}
-        >
+        <a className={styles.link} onClick={handleSupportClick}>
           Pali support
         </a>
+      </span>
+      <span className="text-white">
+        To access the support link, you need to give permission or copy and
+        paste the link below{' '}
+        <div style={{ paddingTop: '8px ' }}>
+          <a className={styles.link}>https://discord.gg/8QKeyurHRd </a>
+          <IconButton
+            style={{ padding: '0 0 0 6px ' }}
+            className={clsx(styles.iconBtn, { [styles.active]: isCopied })}
+            onClick={() => copyText('https://discord.gg/8QKeyurHRd')}
+          >
+            <CopyIcon style={{ color: 'white', fontSize: '1rem' }} />
+          </IconButton>
+        </div>
       </span>
       <span>
         API Docs
