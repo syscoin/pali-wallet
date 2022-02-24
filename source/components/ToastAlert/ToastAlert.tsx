41575f5a2caa373e033cc751ef60cb5493700a2a
Lucas Gauer <lucasfgauer97@gmail.com>
2022-02-24 14:46:44 -0300

    refactor: ToastAlert component cleanup

--- source/components/ToastAlert/ToastAlert.tsx
+++ source/components/ToastAlert/ToastAlert.tsx
@@ -1,50 +1,46 @@
-import React, { FC } from 'react';
+import React from 'react';
 
 import { Icon } from '../Icon';
 
-interface IAlertTemplate {
+interface IToastAlert {
   close: () => void;
-  message: any;
-  options: any;
+  message: string;
+  options: { type: string };
 }
 
-export const ToastAlert: FC<IAlertTemplate> = ({ message, options, close }) => {
-  const otherClasses =
-    options.type === 'error'
-      ? 'bg-warning-error border border-warning-error'
-      : '';
+export const ToastAlert: React.FC<IToastAlert> = ({
+  close,
+  message,
+  options,
+}) => {
+  const isSuccess = options.type === 'success';
 
   return (
-    <>
+    <div
+      className="flex gap-x-2 items-center justify-center mb-16 w-72 h-20 text-center text-brand-white bg-bkg-4 border border-transparent rounded-lg"
+      onClick={close}
+    >
       <div
-        className="flex gap-x-2 items-center justify-center mb-16 w-72 h-20 text-center text-brand-white bg-bkg-4 border border-transparent rounded-lg"
-        onClick={close}
+        className={`${
+          isSuccess
+            ? 'bg-warning-success border border-warning-success'
+            : 'bg-warning-error border border-warning-error'
+        } rounded-l-lg h-full flex justify-center items-center w-1/4`}
+        id="modal-alert"
       >
-        <div
-          className={`${
-            options.type === 'success'
-              ? 'bg-warning-success border border-warning-success'
-              : otherClasses
-          } rounded-l-lg h-full flex justify-center items-center w-1/4`}
-          id="modal-alert"
-        >
-          {options.type === 'success' && (
-            <Icon
-              name={options.type === 'success' ? 'check' : 'close-circle'}
-              className="mb-1 text-brand-white bg-brand-white rounded-full"
-              size={24}
-            />
-          )}
-        </div>
-
-        <div className="w-full text-left">
-          <p className="text-sm font-bold">
-            {options.type === 'success' ? 'Success' : 'Error'}
-          </p>
-
-          <p className="my-2 text-xs">{message}</p>
-        </div>
+        {isSuccess && (
+          <Icon
+            name="check"
+            className="mb-1 text-brand-white bg-brand-white rounded-full"
+            size={24}
+          />
+        )}
       </div>
-    </>
+
+      <div className="w-full text-left">
+        <p className="text-sm font-bold">{isSuccess ? 'Success' : 'Error'}</p>
+        <p className="my-2 text-xs">{message}</p>
+      </div>
+    </div>
   );
 };
