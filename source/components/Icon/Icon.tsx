46d9e78d5f3289d13370c0c21e4aa5c3d83b1290
Lucas Gauer <lucasfgauer97@gmail.com>
2022-02-24 14:04:49 -0300

    refactor: Icon component

--- source/components/Icon/Icon.tsx
+++ source/components/Icon/Icon.tsx
@@ -1,89 +1,50 @@
 import React, { FC } from 'react';
-import {
-  LoadingOutlined,
-  ArrowDownOutlined,
-  CloseCircleOutlined,
-  ReloadOutlined,
-  ArrowUpOutlined,
-  VerticalAlignTopOutlined,
-  CopyOutlined,
-  ArrowLeftOutlined,
-  ExportOutlined,
-  LinkOutlined,
-  ClockCircleOutlined,
-  FileProtectOutlined,
-  WarningOutlined,
-  KeyOutlined,
-  UserOutlined,
-  PartitionOutlined,
-  LockOutlined,
-  MoreOutlined,
-  QuestionCircleOutlined,
-  SettingOutlined,
-  HomeOutlined,
-  DollarOutlined,
-  WalletOutlined,
-  DeleteOutlined,
-  CloseOutlined,
-  SelectOutlined,
-  DownOutlined,
-  UpOutlined,
-  CaretDownOutlined,
-  GlobalOutlined,
-  AppstoreAddOutlined,
-  EditOutlined,
-  CheckCircleOutlined,
-  CaretUpOutlined,
-  MessageOutlined,
-  VerifiedOutlined,
-  CheckOutlined,
-  LaptopOutlined,
-} from '@ant-design/icons';
+import * as AntIcons from '@ant-design/icons';
 
 const icons = {
-  loading: LoadingOutlined,
-  'arrow-down': ArrowDownOutlined,
-  'close-circle': CloseCircleOutlined,
-  reload: ReloadOutlined,
-  'arrow-up': ArrowUpOutlined,
-  'vertical-align': VerticalAlignTopOutlined,
-  copy: CopyOutlined,
-  'arrow-left': ArrowLeftOutlined,
-  export: ExportOutlined,
-  link: LinkOutlined,
-  clock: ClockCircleOutlined,
-  'file-protect': FileProtectOutlined,
-  warning: WarningOutlined,
-  key: KeyOutlined,
-  user: UserOutlined,
-  partition: PartitionOutlined,
-  lock: LockOutlined,
-  dots: MoreOutlined,
-  question: QuestionCircleOutlined,
-  settings: SettingOutlined,
-  home: HomeOutlined,
-  dolar: DollarOutlined,
-  wallet: WalletOutlined,
-  delete: DeleteOutlined,
-  close: CloseOutlined,
-  select: SelectOutlined,
-  down: DownOutlined,
-  up: UpOutlined,
-  'select-down': CaretDownOutlined,
-  globe: GlobalOutlined,
-  appstoreadd: AppstoreAddOutlined,
-  edit: EditOutlined,
-  check: CheckCircleOutlined,
-  'select-up': CaretUpOutlined,
-  message: MessageOutlined,
-  verified: VerifiedOutlined,
-  'check-outlined': CheckOutlined,
-  desktop: LaptopOutlined,
+  appstoreadd: AntIcons.AppstoreAddOutlined,
+  'arrow-down': AntIcons.ArrowDownOutlined,
+  'arrow-left': AntIcons.ArrowLeftOutlined,
+  'arrow-up': AntIcons.ArrowUpOutlined,
+  'check-outlined': AntIcons.CheckOutlined,
+  close: AntIcons.CloseOutlined,
+  'close-circle': AntIcons.CloseCircleOutlined,
+  check: AntIcons.CheckCircleOutlined,
+  clock: AntIcons.ClockCircleOutlined,
+  copy: AntIcons.CopyOutlined,
+  delete: AntIcons.DeleteOutlined,
+  desktop: AntIcons.LaptopOutlined,
+  dolar: AntIcons.DollarOutlined,
+  dots: AntIcons.MoreOutlined,
+  down: AntIcons.DownOutlined,
+  edit: AntIcons.EditOutlined,
+  export: AntIcons.ExportOutlined,
+  'file-protect': AntIcons.FileProtectOutlined,
+  globe: AntIcons.GlobalOutlined,
+  home: AntIcons.HomeOutlined,
+  key: AntIcons.KeyOutlined,
+  link: AntIcons.LinkOutlined,
+  loading: AntIcons.LoadingOutlined,
+  lock: AntIcons.LockOutlined,
+  message: AntIcons.MessageOutlined,
+  partition: AntIcons.PartitionOutlined,
+  question: AntIcons.QuestionCircleOutlined,
+  reload: AntIcons.ReloadOutlined,
+  select: AntIcons.SelectOutlined,
+  'select-down': AntIcons.CaretDownOutlined,
+  'select-up': AntIcons.CaretUpOutlined,
+  settings: AntIcons.SettingOutlined,
+  user: AntIcons.UserOutlined,
+  up: AntIcons.UpOutlined,
+  verified: AntIcons.VerifiedOutlined,
+  'vertical-align': AntIcons.VerticalAlignTopOutlined,
+  wallet: AntIcons.WalletOutlined,
+  warning: AntIcons.WarningOutlined,
 };
 
 interface IIcon {
   className?: string;
-  color?: any;
+  color?: string;
   id?: string;
   name: string;
   rotate?: number;
@@ -92,25 +53,25 @@ interface IIcon {
 }
 
 export const Icon: FC<IIcon> = ({
-  name,
   className,
-  wrapperClassname,
+  color,
+  id,
+  name,
   rotate,
   size,
-  id,
-  color,
+  wrapperClassname,
 }) => {
   const Component = icons[name];
 
   return (
-    <div className={wrapperClassname && wrapperClassname} id={id}>
-      {Component ? (
+    <div className={wrapperClassname} id={id}>
+      {Component && (
         <Component
           className={className}
           style={{ fontSize: size, color }}
           rotate={rotate}
         />
-      ) : null}
+      )}
     </div>
   );
 };
