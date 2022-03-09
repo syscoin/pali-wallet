ab6bd00d34b9d931a052fab0c9df3b7189fbd150
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-09 14:19:11 -0300

    chore: Move utils tests to utils folder

--- source/utils/utils.spec.ts
+++ source/utils/utils.spec.ts
@@ -1,11 +1,13 @@
-import { getHost, isNFT } from '../../source/utils';
+import { getHost, isNFT } from '.';
 
-describe('useUtils test', () => {
+describe('Utils', () => {
+  //* getHost
   it('should get the host from an url', () => {
     const host = getHost('https://test-url.com');
     expect(host).toBe('test-url.com');
   });
 
+  //* isNFT
   it('should check if a given guid is NFT', () => {
     expect(isNFT(5271816415)).toBe(true);
     expect(isNFT(70131121)).toBe(false);
