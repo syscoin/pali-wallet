2e35333abcbbaa4b4f7633d903d1efa6dd138209
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-03-22 10:52:39 -0300

    feat: add ethereum methods in EthereumProvider

--- source/scripts/Provider/EthereumProvider.ts
+++ source/scripts/Provider/EthereumProvider.ts
@@ -1,28 +1,49 @@
+import { Web3Accounts } from '@syspollum/sysweb3-keyring';
+import {
+  getNftImage as web3NFTImage,
+  getTokenIconBySymbol as getTokenIcon,
+} from '@syspollum/sysweb3-utils';
+import { web3Provider as provider } from '@syspollum/sysweb3-network';
+
 export const EthereumProvider = () => {
-  const getNetwork = () => {};
+  const createAccount = () => Web3Accounts().createAccount();
 
-  const getAccounts = () => {};
+  const importAccount = (
+    mnemonicOrPrivateKey: string,
+    encryptedPwdAccount: string
+  ) => Web3Accounts().importAccount(mnemonicOrPrivateKey, encryptedPwdAccount);
 
-  const getChainId = () => {};
+  const sendTransactions = (
+    fromPrivateKey: string,
+    toAddress: string,
+    value: number
+  ) => Web3Accounts().sendTransaction(fromPrivateKey, toAddress, value);
 
-  const getAddress = () => {};
+  const getBalance = (walletAddress: string) =>
+    Web3Accounts().getBalance(walletAddress);
 
-  const getBalance = () => {};
+  const changeNetwork = (networkId: number) =>
+    Web3Accounts().setActiveNetwork(networkId);
 
-  const getXpub = () => {};
-
-  const getTokens = () => {};
-
-  const getState = () => {};
+  const getNFTImage = (NFTContractAddress: string, tokenId: number) =>
+    web3NFTImage(NFTContractAddress, tokenId);
+  const getUserNFT = (walletAddress: string) =>
+    Web3Accounts().getNftsByAddress(walletAddress);
+  const getTokens = (walletAddress: string) =>
+    Web3Accounts().getTokens(walletAddress);
+  const getTokenIconBySymbol = (symbol: string) => getTokenIcon(symbol);
+  const web3Provider = provider;
 
   return {
-    getAccounts,
-    getNetwork,
-    getChainId,
-    getAddress,
+    createAccount,
+    importAccount,
+    sendTransactions,
+    changeNetwork,
+    getNFTImage,
+    getUserNFT,
+    getTokenIconBySymbol,
+    web3Provider,
     getBalance,
-    getXpub,
     getTokens,
-    getState,
   };
 };
