5d0448ef9297817ba4e0834fd66882d55321b32d
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-03-22 14:43:42 -0300

    refactor: change code structure

--- source/scripts/Provider/EthereumProvider.ts
+++ source/scripts/Provider/EthereumProvider.ts
@@ -7,53 +7,24 @@ export const EthereumProvider = () => {
     return currentNetwork;
   };
 
-  const getAccounts = () => {
-    if (window.ethereum) {
-      await window.ethereum.enable();
-      const accounts = await window.ethereum.request({
-        method: 'eth_accounts',
-      });
-
-      return accounts;
-    }
-    return console.log('window.ethereum not found');
+  const getAccounts = async () => {
+    const accounts = await web3Provider.eth.getAccounts();
+    return accounts;
   };
 
   const getChainId = async () => {
-    if (window.ethereum) {
-      await window.ethereum.enable();
-      const chain: number = await window.ethereum.request({
-        method: 'net_version',
-      });
-
-      return chain;
-    }
-    return console.log('window.ethereum not found');
+    const currentChainId = await web3Provider.eth.getChainId();
+    return currentChainId;
   };
 
   const getAddress = async () => {
-    if (window.ethereum) {
-      await window.ethereum.enable();
-      const address = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-
-      return address[0];
-    }
-    return console.log('window.ethereum not found');
+    const address = await web3Provider.eth.getAccounts();
+    return address[0];
   };
 
   const getBalance = async (address: string) => {
-    if (window.ethereum) {
-      await window.ethereum.enable();
-      const balance: number = await window.ethereum.request({
-        method: 'eth_getBalance',
-        params: [address, 'latest'],
-      });
-
-      return balance;
-    }
-    return console.log('window.ethereum not found');
+    const accountBalance = await web3Provider.eth.getBalance(address);
+    return accountBalance;
   };
 
   return {
