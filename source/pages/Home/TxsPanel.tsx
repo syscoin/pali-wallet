49049783604d3de4bf882fe95142d7714b709c63
Amanda Gonsalves <amandagonsalves75@gmail.com>
2022-03-23 12:22:31 -0300

    chore: revert d966c79c6f0d94fdaa43796c69b39b3d1c40625d

--- source/pages/Home/TxsPanel.tsx
+++ source/pages/Home/TxsPanel.tsx
@@ -8,8 +8,8 @@ export const TxsPanel: FC = () => {
   const [isActivity, setActivity] = useState<boolean>(true);
 
   return (
-    <div className="flex flex-col items-center w-full h-72">
-      <div className="flex items-center justify-center w-full text-brand-white text-base border-b border-bkg-6">
+    <div className="flex flex-col items-center w-full h-60 sm:h-72">
+      <div className="flex items-center justify-center w-full text-brand-white text-base border-b border-bkg-1">
         <Button
           className={`flex-1 p-2 ${!isActivity ? 'bg-bkg-2' : 'bg-bkg-1'}`}
           id="assets-btn"
