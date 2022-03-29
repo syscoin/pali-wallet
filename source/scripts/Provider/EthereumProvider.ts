20fc61c4d23f50e5d80404f5997dad8c617a6135
Amanda Gonsalves <amandagonsalves75@gmail.com>
2022-03-29 10:22:13 -0300

    fix: get balance

--- source/scripts/Provider/EthereumProvider.ts
+++ source/scripts/Provider/EthereumProvider.ts
@@ -3,44 +3,42 @@ import { web3Provider } from '@pollum-io/sysweb3-network';
 import { Web3Accounts } from '@pollum-io/sysweb3-keyring';
 import { getController } from 'utils/browser';
 import { useSelector } from 'react-redux';
-import { RootState } from 'state/store';
+import store, { RootState } from 'state/store';
 
 export const EthereumProvider = () => {
+  const { balance } = getConnectedAccount();
+
   const getNetwork = async () => {
     const currentNetwork = await web3Provider.eth.net.getNetworkType();
+
     return currentNetwork;
   };
 
   const getAccounts = async () => {
     const accounts = await web3Provider.eth.getAccounts();
-    return accounts;
-  };
 
-  const getXpub = () => {
-    const controller = getController();
-    const activeAccount = controller.wallet.account.getActiveAccount();
-    return activeAccount?.xpub;
+    return accounts;
   };
 
   const getTokens = async (address: string) => {
     const userTokens = await Web3Accounts().getTokens(address);
+
     return userTokens;
   };
 
   const getChainId = async () => {
     const currentChainId = await web3Provider.eth.getChainId();
+
     return currentChainId;
   };
 
   const getAddress = async () => {
     const address = await web3Provider.eth.getAccounts();
+
     return address[0];
   };
 
-  const getBalance = async (address: string) => {
-    const accountBalance = await web3Provider.eth.getBalance(address);
-    return accountBalance;
-  };
+  const getBalance = async () => balance;
 
   const handleLockAccount = async (walletAddress: string) =>
     web3Provider.eth.personal.lockAccount(walletAddress);
