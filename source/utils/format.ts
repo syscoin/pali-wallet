8833bf58336a30d1aa6c71c67b6eb57cd435bce4
Olavo Ançay <63649496+olavoancay2802@users.noreply.github.com>
2022-04-04 08:02:32 -0300

    test: add formatSeed test and fix formatSeedPhrase

--- source/utils/format.ts
+++ source/utils/format.ts
@@ -72,16 +72,16 @@ export const formatSeedPhrase = (seed: string) => {
 
   const onlyLettersAndSpacesSeed = seed.replace(onlyLettersAndSpacesRegex, ' ');
   const lowerCaseTransform = onlyLettersAndSpacesSeed.toLowerCase();
-
   const seedWithoutDoubleSpaces = lowerCaseTransform.replace(
     withoutDoubleSpacesRegex,
     ' '
   );
-  const seedLength = seedWithoutDoubleSpaces.split(' ').length;
-  if (seedLength === 12) {
-    const formatedSeed = seedWithoutDoubleSpaces.trim();
 
-    return formatedSeed;
+  const formattedSeed = seedWithoutDoubleSpaces.trim();
+  const seedLength = formattedSeed.split(' ').length;
+
+  if (seedLength === 12) {
+    return formattedSeed;
   }
   return seed;
 };
