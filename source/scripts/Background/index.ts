735c6dd6cde601da26b2e0a7612bc881b7198f1d
Lucas Gauer <lucasfgauer97@gmail.com>
2022-06-14 14:53:06 -0300

    fix: Add 'onConnect' listener to setup sysweb3Di

--- source/scripts/Background/index.ts
+++ source/scripts/Background/index.ts
@@ -22,7 +22,9 @@ if (!window.controller) {
 
 browser.runtime.onInstalled.addListener(() => {
   console.emoji('🤩', 'Pali extension enabled');
+});
 
+browser.runtime.onConnect.addListener(() => {
   sysweb3Di.getStateStorageDb().setPrefix('sysweb3-');
   sysweb3Di.useFetchHttpClient(window.fetch.bind(window));
   sysweb3Di.useLocalStorageClient(window.localStorage);
