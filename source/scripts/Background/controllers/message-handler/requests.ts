5fd158e18b36aa8bf2649e6969e1cc7b8cd402bb
Lucas Gauer <lucasfgauer97@gmail.com>
2022-07-19 14:29:44 -0300

    fix: Add 'isConnected' to wallet methods

--- source/scripts/Background/controllers/message-handler/requests.ts
+++ source/scripts/Background/controllers/message-handler/requests.ts
@@ -12,137 +12,33 @@ export const handleRequest = async (
   setPendingWindow: (isPending: boolean) => void,
   isPendingWindow: () => boolean
 ) => {
-  console.log('[request handler] data:', message.data);
-  const { args } = message.data;
+  const { dapp } = window.controller;
+
   const [prefix, methodName] = message.data.method.split('_');
-
-  const { controller } = window;
-
-  // const isConnected = controller.dapp.isDAppConnected(origin);
-  // const walletIsLocked = !controller.wallet.isUnlocked();
-
-  // TODO improve this
-  if (prefix === 'wallet' && methodName === 'changeAccount')
-    return changeAccount(
-      message.data.network,
-      origin,
-      isPendingWindow,
-      setPendingWindow
-    );
-
-  const provider =
-    prefix === 'sys'
-      ? controller.dapp.sysProvider
-      : controller.dapp.ethProvider;
-
-  /* const windowId = `signMessage${uuid()}`;
-
-  const isSignMessage = async () => {
-    if (isPendingWindow()) {
-      return Promise.resolve(null);
+  if (prefix === 'wallet') {
+    switch (methodName) {
+      case 'isConnected':
+        const isWhitelisted = dapp.isDAppConnected(origin);
+        const hasConnectedAccount = dapp.hasConnectedAccount();
+        return isWhitelisted && hasConnectedAccount;
+      case 'changeAccount':
+        return changeAccount(
+          message.data.network,
+          origin,
+          isPendingWindow,
+          setPendingWindow
+        );
+      default:
+        throw new Error('Unknown method');
     }
+  }
 
-    const popup = await controller.createPopup(windowId);
-    setPendingWindow(true);
-
-    controller.dapp.setSigRequest({
-      origin: origin as string,
-      address: args[1],
-      message: args[0],
-    });
-
-    window.addEventListener(
-      'sign',
-      (ev: any) => {
-        if (ev.detail.substring(1) === windowId) {
-          result = controller.dapp.paliProvider.signMessage(args[0]);
-          port.postMessage({ id: message.id, data: { result } });
-          setPendingWindow(false);
-        }
-      },
-      {
-        once: true,
-        passive: true,
-      }
-    );
-
-    browser.windows.onRemoved.addListener((id) => {
-      if (popup && id === popup.id) {
-        port.postMessage({ id: message.id, data: { result: false } });
-        setPendingWindow(false);
-      }
-    });
-
-    return Promise.resolve(null);
-  };
-
-  const sendTransaction = async (data) => {
-    await controller.createPopup(
-      windowId,
-      message.data.network,
-      'sendTransaction',
-      { ...data }
-    );
-
-    setPendingWindow(true);
-
-    window.addEventListener(
-      'transactionSent',
-      (ev: any) => {
-        if (ev.detail.windowId === windowId) {
-          port.postMessage({
-            id: message.id,
-            data: { result: true, data: {} },
-          });
-          setPendingWindow(false);
-        }
-      },
-      { once: true, passive: true }
-    );
-  };
-
-  const approveSpend = async (data) => {
-    await controller.createPopup(
-      windowId,
-      message.data.network,
-      'approveSpend',
-      { ...data }
-    );
-
-    setPendingWindow(true);
-
-    window.addEventListener(
-      'spendApproved',
-      (ev: any) => {
-        if (ev.detail.windowId === windowId) {
-          port.postMessage({
-            id: message.id,
-            data: { result: true, data: {} },
-          });
-          setPendingWindow(false);
-        }
-      },
-      { once: true, passive: true }
-    );
-  };
-
-  const isSendTransaction = () => {
-    const data = args[0] ? args[0][0] : {};
-    const decoder = erc20DataDecoder();
-    const decodedTxData = data?.data ? decoder.decodeData(data?.data) : null;
-
-    if (decodedTxData?.method === 'approve') {
-      return approveSpend(data);
-    }
-
-    return sendTransaction(data);
-  }; */
-
+  const provider = prefix === 'sys' ? dapp.sysProvider : dapp.ethProvider;
   const method = provider[methodName];
-  if (!method) throw new Error('Unknown method');
-  const result = await method(args);
 
-  return result;
+  if (!method) throw new Error('Unknown method');
+
+  return await method(message.data.args);
 };
 
 const changeAccount = async (
