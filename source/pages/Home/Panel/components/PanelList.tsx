184be3fdb64e73046ddf867b7c742c01e0d1b773
Lucas Gauer <lucasfgauer97@gmail.com>
2022-07-28 13:53:00 -0300

    fix: home/details routing on activity panel

--- source/pages/Home/Panel/components/PanelList.tsx
+++ source/pages/Home/Panel/components/PanelList.tsx
@@ -128,7 +128,7 @@ export const PanelList: FC<IPanelList> = ({
                     <IconButton
                       className="w-5"
                       onClick={() =>
-                        navigate('/home/tx-details/', {
+                        navigate('/home/details/', {
                           state: {
                             tx,
                             type: getTxType(tx),
