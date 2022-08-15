5324769723503820c341932a1c0720eec3d8be53
Amanda Gonsalves <amandagonsalvesags@gmail.com>
2022-08-15 09:30:01 -0300

    chore: unshift assets details list

--- source/pages/Home/Panel/components/Assets/SyscoinDetails.tsx
+++ source/pages/Home/Panel/components/Assets/SyscoinDetails.tsx
@@ -50,7 +50,7 @@ export const SyscoinAssetDetais = ({ id }: { id: string }) => {
 
       const isValid = typeof value !== 'object';
 
-      if (isValid) formattedAsset.push(formattedValue);
+      if (isValid) formattedAsset.unshift(formattedValue);
     }
 
     return formattedAsset;
