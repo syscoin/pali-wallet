14b4bab47fabe8dddc7d43c575ba41f1fea5005c
Lucas Gauer <lucasfgauer97@gmail.com>
2022-08-16 13:46:20 -0300

    chore: Update `isActiveNetwork`

--- source/utils/network.ts
+++ source/utils/network.ts
@@ -1,8 +1,10 @@
 import store from 'state/store';
 
 export const isActiveNetwork = (chain: string, chainId: number) => {
-  const { activeNetwork } = store.getState().vault;
-  const isSysCore = activeNetwork.url.includes('blockbook');
+  const { activeNetwork, networks } = store.getState().vault;
+  const isSysCore =
+    Boolean(networks.syscoin[activeNetwork.chainId]) &&
+    activeNetwork.url.includes('blockbook');
   const activeChain = isSysCore ? 'syscoin' : 'ethereum';
 
   const isSameChain = chain === activeChain;
