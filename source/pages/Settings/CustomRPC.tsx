9cc648dbf37644f658041de3b06eef8ff88bbcdb
Amanda Gonsalves <amandagonsalvesags@gmail.com>
2022-08-19 22:45:09 -0300

    style: add edit icon and fix routes issues

--- source/pages/Settings/CustomRPC.tsx
+++ source/pages/Settings/CustomRPC.tsx
@@ -11,18 +11,15 @@ import { useUtils } from 'hooks/index';
 import { ICustomRpcParams } from 'types/transactions';
 import { getController } from 'utils/browser';
 
-import { ManageNetwork } from '.';
-
 const CustomRPCView = () => {
   const { state }: { state: any } = useLocation();
 
   const isSyscoinSelected = state && state.chain && state.chain === 'syscoin';
   const [loading, setLoading] = useState(false);
-  const [success, setSuccess] = useState(false);
   const [isUrlValid, setIsUrlValid] = useState(false);
   const [isSyscoinRpc, setIsSyscoinRpc] = useState(Boolean(isSyscoinSelected));
 
-  const { alert } = useUtils();
+  const { alert, navigate } = useUtils();
   const controller = getController();
 
   const [form] = useForm();
@@ -45,8 +42,9 @@ const CustomRPCView = () => {
 
         alert.success('RPC successfully added.');
 
-        setSuccess(true);
         setLoading(false);
+
+        navigate('/settings/networks/edit');
 
         return;
       }
@@ -55,8 +53,9 @@ const CustomRPCView = () => {
 
       alert.success('RPC successfully edited.');
 
-      setSuccess(true);
       setLoading(false);
+
+      navigate('/settings/networks/edit');
     } catch (error: any) {
       alert.removeAll();
       alert.error(error.message);
@@ -72,171 +71,161 @@ const CustomRPCView = () => {
   };
 
   return (
-    <>
-      {success ? (
-        <ManageNetwork />
-      ) : (
-        <Layout title="CUSTOM RPC">
-          <Form
-            form={form}
-            validateMessages={{ default: '' }}
-            id="rpc"
-            name="rpc"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 8 }}
-            initialValues={initialValues}
-            onFinish={onSubmit}
-            autoComplete="off"
-            className="standard flex flex-col gap-2 items-center justify-center text-center"
-          >
-            <Form.Item
-              id="network-switch"
-              name="network-switch"
-              rules={[
-                {
-                  required: false,
-                  message: '',
-                },
-              ]}
+    <Layout title="CUSTOM RPC">
+      <Form
+        form={form}
+        validateMessages={{ default: '' }}
+        id="rpc"
+        name="rpc"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+        initialValues={initialValues}
+        onFinish={onSubmit}
+        autoComplete="off"
+        className="standard flex flex-col gap-2 items-center justify-center text-center"
+      >
+        <Form.Item
+          id="network-switch"
+          name="network-switch"
+          rules={[
+            {
+              required: false,
+              message: '',
+            },
+          ]}
+        >
+          <div className="flex gap-x-2 my-4 text-xs">
+            <p>Ethereum</p>
+
+            <Switch
+              checked={isSyscoinRpc}
+              onChange={() => setIsSyscoinRpc(!isSyscoinRpc)}
+              className="relative inline-flex items-center w-9 h-4 border border-brand-royalblue rounded-full"
             >
-              <div className="flex gap-x-2 my-4 text-xs">
-                <p>Ethereum</p>
-
-                <Switch
-                  checked={isSyscoinRpc}
-                  onChange={() => setIsSyscoinRpc(!isSyscoinRpc)}
-                  className="relative inline-flex items-center w-9 h-4 border border-brand-royalblue rounded-full"
-                >
-                  <span className="sr-only">Syscoin Network</span>
-                  <span
-                    className={`${
-                      isSyscoinRpc
-                        ? 'translate-x-6 bg-brand-royalblue'
-                        : 'translate-x-1 bg-brand-deepPink100'
-                    } inline-block w-2 h-2 transform rounded-full`}
-                  />
-                </Switch>
-
-                <p>Syscoin</p>
-              </div>
-            </Form.Item>
-
-            <Form.Item
-              name="label"
-              className="md:w-full"
-              hasFeedback
-              rules={[
-                {
-                  required: false,
-                  message: '',
-                },
-              ]}
-            >
-              <Input
-                type="text"
-                placeholder="Label (optional)"
-                className="large"
+              <span className="sr-only">Syscoin Network</span>
+              <span
+                className={`${
+                  isSyscoinRpc
+                    ? 'translate-x-6 bg-brand-royalblue'
+                    : 'translate-x-1 bg-brand-deepPink100'
+                } inline-block w-2 h-2 transform rounded-full`}
               />
-            </Form.Item>
+            </Switch>
 
-            <Form.Item
-              name="url"
-              className="md:w-full"
-              hasFeedback
-              rules={[
-                {
-                  required: true,
-                  message: '',
-                },
-                () => ({
-                  async validator(_, value) {
-                    if (isSyscoinRpc) {
-                      const { valid, coin } = await validateSysRpc(value);
+            <p>Syscoin</p>
+          </div>
+        </Form.Item>
 
-                      if (valid || !value) {
-                        populateForm('label', String(coin));
+        <Form.Item
+          name="label"
+          className="md:w-full"
+          hasFeedback
+          rules={[
+            {
+              required: false,
+              message: '',
+            },
+          ]}
+        >
+          <Input type="text" placeholder="Label (optional)" className="large" />
+        </Form.Item>
 
-                        return Promise.resolve();
-                      }
+        <Form.Item
+          name="url"
+          className="md:w-full"
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: '',
+            },
+            () => ({
+              async validator(_, value) {
+                if (isSyscoinRpc) {
+                  const { valid, coin } = await validateSysRpc(value);
 
-                      return Promise.reject();
-                    }
+                  if (valid || !value) {
+                    populateForm('label', String(coin));
 
-                    const { valid, details } = await validateEthRpc(value);
+                    return Promise.resolve();
+                  }
 
-                    setIsUrlValid(valid);
+                  return Promise.reject();
+                }
 
-                    if (valid || !value) {
-                      populateForm('label', String(details.name));
-                      populateForm('chainId', String(details.chainId));
+                const { valid, details } = await validateEthRpc(value);
 
-                      return Promise.resolve();
-                    }
+                setIsUrlValid(valid);
 
-                    return Promise.reject();
-                  },
-                }),
-              ]}
-            >
-              <Input
-                type="text"
-                placeholder={`${
-                  isSyscoinRpc ? 'Trezor Block Explorer' : 'RPC URL'
-                }`}
-                className="large"
-              />
-            </Form.Item>
+                if (valid || !value) {
+                  populateForm('label', String(details.name));
+                  populateForm('chainId', String(details.chainId));
 
-            <Form.Item
-              name="chainId"
-              hasFeedback
-              className="md:w-full"
-              rules={[
-                {
-                  required: !isSyscoinRpc,
-                  message: '',
-                },
-              ]}
-            >
-              <Input
-                type="text"
-                disabled={!form.getFieldValue('url') || isUrlValid}
-                placeholder="Chain ID"
-                className={`${isSyscoinRpc ? 'hidden' : 'block'} large`}
-              />
-            </Form.Item>
+                  return Promise.resolve();
+                }
 
-            <Form.Item
-              hasFeedback
-              className="md:w-full"
-              name="apiUrl"
-              rules={[
-                {
-                  required: false,
-                  message: '',
-                },
-              ]}
-            >
-              <Input
-                type="text"
-                placeholder="API URL (optional)"
-                className={`${isSyscoinRpc ? 'hidden' : 'block'} large`}
-              />
-            </Form.Item>
+                return Promise.reject();
+              },
+            }),
+          ]}
+        >
+          <Input
+            type="text"
+            placeholder={`${
+              isSyscoinRpc ? 'Trezor Block Explorer' : 'RPC URL'
+            }`}
+            className="large"
+          />
+        </Form.Item>
 
-            <p className="px-8 py-4 text-center text-brand-white font-poppins text-sm">
-              You can edit this later if you need on network settings menu.
-            </p>
+        <Form.Item
+          name="chainId"
+          hasFeedback
+          className="md:w-full"
+          rules={[
+            {
+              required: !isSyscoinRpc,
+              message: '',
+            },
+          ]}
+        >
+          <Input
+            type="text"
+            disabled={!form.getFieldValue('url') || isUrlValid}
+            placeholder="Chain ID"
+            className={`${isSyscoinRpc ? 'hidden' : 'block'} large`}
+          />
+        </Form.Item>
 
-            <div className="absolute bottom-12 md:static">
-              <SecondaryButton type="submit" loading={loading}>
-                Save
-              </SecondaryButton>
-            </div>
-          </Form>
-        </Layout>
-      )}
-    </>
+        <Form.Item
+          hasFeedback
+          className="md:w-full"
+          name="apiUrl"
+          rules={[
+            {
+              required: false,
+              message: '',
+            },
+          ]}
+        >
+          <Input
+            type="text"
+            placeholder="API URL (optional)"
+            className={`${isSyscoinRpc ? 'hidden' : 'block'} large`}
+          />
+        </Form.Item>
+
+        <p className="px-8 py-4 text-center text-brand-white font-poppins text-sm">
+          You can edit this later if you need on network settings menu.
+        </p>
+
+        <div className="absolute bottom-12 md:static">
+          <SecondaryButton type="submit" loading={loading}>
+            Save
+          </SecondaryButton>
+        </div>
+      </Form>
+    </Layout>
   );
 };
 
