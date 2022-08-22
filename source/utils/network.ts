910b5a3247102ad4bd4cbeef4181f3aa6b074fed
Lucas Gauer <lucasfgauer97@gmail.com>
2022-08-22 12:35:42 -0300

    chore: Create `isSysNetwork` and `networkChain`

--- source/utils/network.ts
+++ source/utils/network.ts
@@ -1,14 +1,31 @@
 import store from 'state/store';
 
 export const isActiveNetwork = (chain: string, chainId: number) => {
-  const { activeNetwork, networks } = store.getState().vault;
-  const isSysCore =
-    Boolean(networks.syscoin[activeNetwork.chainId]) &&
-    activeNetwork.url.includes('blockbook');
-  const activeChain = isSysCore ? 'syscoin' : 'ethereum';
+  const { activeNetwork } = store.getState().vault;
+  const activeChain = networkChain(activeNetwork);
 
   const isSameChain = chain === activeChain;
   const isSameChainId = activeNetwork.chainId === chainId;
 
   return isSameChain && isSameChainId;
 };
+
+/**
+ * `{ chaindId, url }` is compatible with `INetwork`
+ */
+export const isSysNetwork = ({
+  chainId,
+  url,
+}: {
+  chainId: number;
+  url: string;
+}) => {
+  const { networks } = store.getState().vault;
+  return Boolean(networks.syscoin[chainId]) && url.includes('blockbook');
+};
+
+/**
+ * `{ chaindId, url }` is compatible with `INetwork`
+ */
+export const networkChain = (network: { chainId: number; url: string }) =>
+  isSysNetwork(network) ? 'syscoin' : 'ethereum';
