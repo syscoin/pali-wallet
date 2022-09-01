c3d6f4004ad7f5db49cdf655c2580bed760d78d3
Lucas Gauer <lucasfgauer97@gmail.com>
2022-09-01 11:37:10 -0300

    chore: Remove unused code related to dapps event listeners

--- source/state/dapp/dapp.spec.ts
+++ source/state/dapp/dapp.spec.ts
@@ -1,89 +1,44 @@
 import reducer, {
-  addListener,
-  removeListener,
   addDApp,
   removeDApp,
   updateDAppAccount,
   initialState,
-  removeListeners,
 } from '.';
 import { IDApp } from './types';
 
 /*  ----- Tests ----- */
 
 describe('dapp store actions', () => {
-  describe('Listeners tests', () => {
-    //* addListener
-    it('should add a listener', () => {
-      const payload = {
-        eventName: 'fake event',
-        host: 'fake host',
-      };
-      const newState = reducer(initialState, addListener(payload));
-      expect(newState.listeners[payload.host]).toContain(payload.eventName);
-    });
+  const FAKE_DAPP: IDApp = {
+    host: 'fakehost.net',
+    chain: 'syscoin',
+    chainId: 57,
+    accountId: 0,
+  };
 
-    //* removeListener
-    it('should remove a listener ', () => {
-      const payload = {
-        eventName: 'fake event',
-        host: 'fake host',
-      };
+  //* addDApp
+  it('should add a dapp', () => {
+    const newState = reducer(initialState, addDApp(FAKE_DAPP));
 
-      const customState = reducer(initialState, addListener(payload));
-
-      const newState = reducer(customState, removeListener(payload));
-
-      expect(newState.listeners).not.toContain(payload.host);
-    });
-
-    //* removeListeners
-    it('should remove a listener ', () => {
-      const host = 'hostname.com';
-      const event1 = { eventName: 'event1', host };
-      const event2 = { eventName: 'event2', host };
-
-      let customState = reducer(initialState, addListener(event1));
-      customState = reducer(customState, addListener(event2));
-
-      const newState = reducer(customState, removeListeners(host));
-
-      expect(newState.listeners[host]).toBeUndefined();
-    });
+    expect(newState.dapps[FAKE_DAPP.host]).toEqual(FAKE_DAPP);
   });
 
-  describe('Dapp tests', () => {
-    const FAKE_DAPP: IDApp = {
-      host: 'fakehost.net',
-      chain: 'syscoin',
-      chainId: 57,
-      accountId: 0,
-    };
+  //* removeDApp
+  it('should remove a dapp', () => {
+    const customState = reducer(initialState, addDApp(FAKE_DAPP));
+    const newState = reducer(customState, removeDApp(FAKE_DAPP.host));
 
-    //* addDApp
-    it('should add a dapp', () => {
-      const newState = reducer(initialState, addDApp(FAKE_DAPP));
+    expect(newState.dapps).toEqual(initialState.dapps);
+  });
 
-      expect(newState.dapps[FAKE_DAPP.host]).toEqual(FAKE_DAPP);
-    });
+  //* updateDAppAccount
+  it('should remove a dapp', () => {
+    const payload = { host: FAKE_DAPP.host, accountId: 1 };
 
-    //* removeDApp
-    it('should remove a dapp', () => {
-      const customState = reducer(initialState, addDApp(FAKE_DAPP));
-      const newState = reducer(customState, removeDApp(FAKE_DAPP.host));
+    const customState = reducer(initialState, addDApp(FAKE_DAPP));
+    const newState = reducer(customState, updateDAppAccount(payload));
 
-      expect(newState.dapps).toEqual(initialState.dapps);
-    });
-
-    //* updateDAppAccount
-    it('should remove a dapp', () => {
-      const payload = { host: FAKE_DAPP.host, accountId: 1 };
-
-      const customState = reducer(initialState, addDApp(FAKE_DAPP));
-      const newState = reducer(customState, updateDAppAccount(payload));
-
-      const dapp = newState.dapps[FAKE_DAPP.host];
-      expect(dapp.accountId).toEqual(payload.accountId);
-    });
+    const dapp = newState.dapps[FAKE_DAPP.host];
+    expect(dapp.accountId).toEqual(payload.accountId);
   });
 });
--- source/state/dapp/index.ts
+++ source/state/dapp/index.ts
@@ -4,41 +4,12 @@ import { IDAppState, IDApp } from './types';
 
 export const initialState: IDAppState = {
   dapps: {},
-  listeners: {},
 };
 
 const DAppState = createSlice({
   name: 'dapp',
   initialState,
   reducers: {
-    addListener(
-      state: IDAppState,
-      action: PayloadAction<{ eventName: string; host: string }>
-    ) {
-      const { host, eventName } = action.payload;
-
-      if (!state.listeners[host]) state.listeners[host] = [eventName];
-      if (state.listeners[host].includes(eventName)) return;
-
-      state.listeners[host].push(eventName);
-    },
-
-    removeListener(
-      state: IDAppState,
-      action: PayloadAction<{ eventName: string; host: string }>
-    ) {
-      const { host, eventName } = action.payload;
-
-      const index = state.listeners[host].findIndex((e) => e === eventName);
-      if (index === -1) return;
-
-      state.listeners[host].splice(index, 1);
-    },
-
-    removeListeners(state: IDAppState, action: PayloadAction<string>) {
-      delete state.listeners[action.payload];
-    },
-
     addDApp(state: IDAppState, action: PayloadAction<IDApp>) {
       state.dapps[action.payload.host] = action.payload;
     },
@@ -61,13 +32,6 @@ const DAppState = createSlice({
   },
 });
 
-export const {
-  addDApp,
-  removeDApp,
-  updateDAppAccount,
-  addListener,
-  removeListener,
-  removeListeners,
-} = DAppState.actions;
+export const { addDApp, removeDApp, updateDAppAccount } = DAppState.actions;
 
 export default DAppState.reducer;
