552d030895a5f6b845142078fe3b9ef77d315507
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-09-12 15:13:27 -0300

    fix: solve prettier issue

--- source/pages/Transactions/TransactionConfirmation.tsx
+++ source/pages/Transactions/TransactionConfirmation.tsx
@@ -152,7 +152,8 @@ const TransactionConfirmation: React.FC<ITransactionConfirmation> = ({
                     {typeof item.value === 'string' && item.value.length > 10
                       ? ellipsis(item.value)
                       : item.value}
-                    {typeof item.value === 'boolean' && (item.value ? 'Yes' : 'No')}
+                    {typeof item.value === 'boolean' &&
+                      (item.value ? 'Yes' : 'No')}
                     {!item.value && '-'}
                   </p>
                 </li>
