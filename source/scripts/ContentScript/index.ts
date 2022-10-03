fd7b717af58a5b72e3db92b869f9d4944133022f
Lucas Gauer <lucasfgauer97@gmail.com>
2022-10-03 11:22:14 -0300

    refactor: Merge provider/index into contentscript/index

--- source/scripts/ContentScript/index.ts
+++ source/scripts/ContentScript/index.ts
@@ -1,9 +1,71 @@
-import { provider } from '../Provider/index';
+import { EventEmitter } from 'events';
+import { browser } from 'webextension-polyfill-ts';
+
 import { DAppMethods } from 'scripts/Background/controllers/message-handler/types';
 
 import { inject as _inject } from './inject';
 
-provider.start();
+// Runs at the page environment
+
+const emitter = new EventEmitter();
+
+// Connect to pali
+const backgroundPort = browser.runtime.connect(undefined, {
+  name: 'pali-inject',
+});
+
+// Every message from pali emits an event
+backgroundPort.onMessage.addListener(({ id, data }) => {
+  emitter.emit(id, data);
+});
+
+// Add listener for pali events
+const checkForPaliRegisterEvent = (type, id) => {
+  if (type === 'EVENT_REG') {
+    emitter.on(id, (result) => {
+      window.dispatchEvent(
+        new CustomEvent(id, { detail: JSON.stringify(result) })
+      );
+    });
+
+    return;
+  }
+
+  emitter.once(id, (result) => {
+    window.dispatchEvent(
+      new CustomEvent(id, { detail: JSON.stringify(result) })
+    );
+  });
+};
+
+/**
+ * Listens to local messages and sends them to pali
+ */
+const start = () => {
+  window.addEventListener(
+    'message',
+    (event) => {
+      if (event.source !== window) return;
+      if (!event.data) return;
+
+      const { id, type, data } = event.data;
+
+      if (!id || !type) return;
+
+      // listen for the response
+      checkForPaliRegisterEvent(type, id);
+
+      backgroundPort.postMessage({
+        id,
+        type,
+        data,
+      });
+    },
+    false
+  );
+};
+
+start();
 
 const inject = (content: string) => {
   const container = document.head || document.documentElement;
