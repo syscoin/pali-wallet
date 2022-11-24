4938b3fe1786c5d108aa774134f10e980b03228c
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-11-24 00:35:22 -0300

    feat: add editPriority modal

--- source/pages/Send/index.tsx
+++ source/pages/Send/index.tsx
@@ -2,6 +2,6 @@ export { SendConfirm } from './Confirm';
 export { SendTransaction } from './SendTransaction';
 export { SendEth } from './SendEth';
 export { SendSys } from './SendSys';
-export { EditPriorityFee } from './EditPriority';
 export { EditPermissionApprovedValueModal } from './EditPermissionApprovedValueModal';
 export { ApproveTransactionComponent } from './Approve';
+export { EditPriorityModal } from './EditPriorityModal';
