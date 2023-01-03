68ae4cbc8476f07dbcba50fb50ba28be74826a6a
Amanda Gonsalves <amandagonsalvesags@gmail.com>
2023-01-02 21:24:25 -0300

    feat: confirm phrase

--- source/pages/SeedConfirm/ConfirmPhrase.tsx
+++ source/pages/SeedConfirm/ConfirmPhrase.tsx
@@ -32,6 +32,7 @@ export const ConfirmPhrase = ({
 
   const handleNewPhrase = (idx: number) => {
     const tempList = [...newList];
+
     setOrgList([...orgList, newList[idx]]);
     tempList.splice(idx, 1);
     setNewList([...tempList]);
@@ -39,41 +40,42 @@ export const ConfirmPhrase = ({
 
   return (
     <OnboardingLayout title="Confirm Recovery Phrase">
-      <div className="flex flex-col gap-4 items-center justify-center mt-2 text-brand-white transition-all duration-300 ease-in-out">
-        <>
-          <section className="flex flex-wrap gap-3 items-center justify-center p-3 w-11/12 border-b border-brand-graylight box-border transition-all duration-300 md:w-9/12">
-            {newList.map((phrase, idx) => (
-              <Button
-                useDefaultWidth={false}
-                className="flex gap-4 items-center justify-center px-3 py-1 min-w-xs h-7 text-brand-white text-xs font-bold tracking-normal leading-4 bg-brand-royalblue border border-brand-royalblue rounded-md"
-                key={phrase}
-                type="button"
-                onClick={() => handleNewPhrase(idx)}
-              >
-                {phrase}
-              </Button>
-            ))}
-          </section>
-          <section className="flex flex-wrap gap-3 items-center justify-center w-11/12 box-border transition-all duration-300 md:w-9/12">
-            {orgList.map((phrase, idx) => (
-              <Button
-                useDefaultWidth={false}
-                className="flex gap-4 items-center justify-center px-3 py-1 min-w-xs h-7 text-brand-white text-xs font-bold tracking-normal leading-4 bg-bkg-2 border border-bkg-4 rounded-md"
-                key={phrase}
-                type="button"
-                onClick={() => handleOrgPhrase(idx)}
-              >
-                {phrase}
-              </Button>
-            ))}
-          </section>
+      <div className="flex flex-col gap-4 items-center justify-start w-full max-w-xs h-full text-center md:max-w-md">
+        <section className="flex flex-wrap gap-3 items-center justify-center p-3 w-11/12 border-b border-brand-graylight box-border transition-all duration-300 md:w-9/12">
+          {newList.map((phrase, idx) => (
+            <Button
+              useDefaultWidth={false}
+              className="flex gap-4 items-center justify-center px-3 py-1 min-w-xs h-7 text-brand-white text-xs font-bold tracking-normal leading-4 bg-brand-royalblue border border-brand-royalblue rounded-md"
+              key={phrase}
+              type="button"
+              onClick={() => handleNewPhrase(idx)}
+            >
+              {phrase}
+            </Button>
+          ))}
+        </section>
 
-          <div className="absolute bottom-12">
-            <PrimaryButton type="button" onClick={confirmPassed}>
-              Validate
-            </PrimaryButton>
-          </div>
-        </>
+        <section className="flex flex-wrap gap-3 items-center justify-center w-11/12 box-border transition-all duration-300 md:w-9/12">
+          {orgList.map((phrase, idx) => (
+            <Button
+              useDefaultWidth={false}
+              className="flex gap-4 items-center justify-center px-3 py-1 min-w-xs h-7 text-brand-white text-xs font-bold tracking-normal leading-4 bg-bkg-2 border border-bkg-4 rounded-md"
+              key={phrase}
+              type="button"
+              onClick={() => handleOrgPhrase(idx)}
+            >
+              {phrase}
+            </Button>
+          ))}
+        </section>
+
+        <PrimaryButton
+          className="absolute bottom-12"
+          type="button"
+          onClick={confirmPassed}
+        >
+          Validate
+        </PrimaryButton>
 
         <DefaultModal
           show={passed}
