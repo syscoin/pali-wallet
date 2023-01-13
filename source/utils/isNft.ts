8e464317fa9ec0545d38c984538a4f1760149c19
leozera14 <leo_faleiros@hotmail.com>
2023-01-13 18:02:33 -0300

    fix: Fix Syscoin send component error

--- source/utils/isNft.ts
+++ source/utils/isNft.ts
@@ -1,4 +1,6 @@
 export const isNFT = (guid: number): boolean => {
+  console.log('GUID RECEIVED', guid);
+
   const assetGuid = BigInt.asUintN(64, BigInt(guid));
   return assetGuid >> BigInt(32) > 0;
 };
