cfdfe84273f817801c27555d29452b42461e4c0a
leozera14 <leo_faleiros@hotmail.com>
2023-01-13 18:08:03 -0300

    chore: remove console.log

--- source/utils/isNft.ts
+++ source/utils/isNft.ts
@@ -1,6 +1,4 @@
 export const isNFT = (guid: number): boolean => {
-  console.log('GUID RECEIVED', guid);
-
   const assetGuid = BigInt.asUintN(64, BigInt(guid));
   return assetGuid >> BigInt(32) > 0;
 };
