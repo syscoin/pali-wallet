f98417fa9a8c7c7a6c7a9a557774fe52130594c5
Lucas Lovato <lucas.lovato@gmail.com>
2023-04-03 23:12:56 -0300

    fix: uniq evm tx

--- source/scripts/Background/controllers/transactions/utils.ts
+++ source/scripts/Background/controllers/transactions/utils.ts
@@ -63,37 +63,39 @@ export const findUserTxsInProviderByBlocksRange = async (
 };
 
 const treatDuplicatedTxs = (transactions: IEvmTransactionResponse[]) =>
-  uniqWith(
-    transactions,
-    (a: IEvmTransactionResponse, b: IEvmTransactionResponse) => {
-      if (a.hash.toLowerCase() === b.hash.toLowerCase()) {
-        console.log('here inside', {
+  uniqBy(
+    uniqWith(
+      transactions,
+      (a: IEvmTransactionResponse, b: IEvmTransactionResponse) => {
+        if (a.hash.toLowerCase() === b.hash.toLowerCase()) {
+          console.log('here inside', {
+            a,
+            b,
+          });
+          // Keep the transaction with the higher confirmation number
+          if (a.confirmations > b.confirmations) {
+            // Preserve timestamp if available
+            if (b.timestamp && !a.timestamp) {
+              a.timestamp = b.timestamp;
+            }
+            return true; // a should be considered equal to b (b will be removed)
+          } else {
+            // Preserve timestamp if available
+            if (a.timestamp && !b.timestamp) {
+              b.timestamp = a.timestamp;
+            }
+            return false; // a should not be considered equal to b (a will be removed)
+          }
+        }
+
+        console.log('here outside', {
           a,
           b,
         });
-        // Keep the transaction with the higher confirmation number
-        if (a.confirmations > b.confirmations) {
-          // Preserve timestamp if available
-          if (b.timestamp && !a.timestamp) {
-            a.timestamp = b.timestamp;
-          }
-          return true; // a should be considered equal to b (b will be removed)
-        } else {
-          // Preserve timestamp if available
-          if (a.timestamp && !b.timestamp) {
-            b.timestamp = a.timestamp;
-          }
-          return false; // a should not be considered equal to b (a will be removed)
-        }
+        return false; // a and b are not equal (both will be kept)
       }
-
-      console.log('here outside', {
-        a,
-        b,
-      });
-
-      return false; // a and b are not equal (both will be kept)
-    }
+    ),
+    'hash'
   );
 
 export const validateAndManageUserTransactions = (
