ca6eec8e391e4b8547736265796ce4cb7a97bb38
Lucas Santos <lucasgabrielgtx@gmail.com>
2023-04-06 20:16:56 -0300

    fix: change ui structure and solve error message issue

--- source/pages/Connections/ChangeAccount.tsx
+++ source/pages/Connections/ChangeAccount.tsx
@@ -3,18 +3,18 @@ import { useSelector } from 'react-redux';
 
 import { KeyringAccountType } from '@pollum-io/sysweb3-keyring';
 
-import { Layout, SecondaryButton, PrimaryButton } from 'components/index';
+import { Layout, SecondaryButton, PrimaryButton, Icon } from 'components/index';
 import { useQueryData } from 'hooks/index';
 import { RootState } from 'state/store';
 import { getController } from 'utils/browser';
 import { ellipsis } from 'utils/index';
 
 export const ChangeAccount = () => {
-  const accounts = useSelector((state: RootState) => state.vault.accounts);
+  const { accounts, isBitcoinBased } = useSelector(
+    (state: RootState) => state.vault
+  );
   const { dapp, wallet } = getController();
   const { host, eventName } = useQueryData();
-  const importedAccounts = Object.values(accounts.Imported);
-  const hdAccounts = Object.values(accounts.HDAccount);
 
   const currentAccountId = dapp.get(host).accountId;
   const currentAccountType = dapp.get(host).accountType;
@@ -39,64 +39,84 @@ export const ChangeAccount = () => {
 
   return (
     <Layout canGoBack={false} title="CONNECTED ACCOUNT" titleOnly={true}>
-      <div className="flex flex-col items-center justify-center w-full">
-        <h1 className="text-sm">PALI WALLET</h1>
-        <h2>Pali Accounts</h2>
-        <ul className="scrollbar-styled flex flex-col gap-4 mt-4 px-8 w-full h-72 overflow-auto">
-          {Object.values(hdAccounts).map((account) => (
-            <li
-              className={`${
-                account.id === currentAccountId
-                  ? 'cursor-not-allowed bg-opacity-50 border-brand-royalblue'
-                  : 'cursor-pointer hover:bg-bkg-4 border-brand-royalblue'
-              } border border-solid  rounded-lg px-2 py-4 text-xs bg-bkg-2 flex justify-between items-center transition-all duration-200`}
-              key={account.id}
-              onClick={() =>
-                handleSetAccountId(account.id, KeyringAccountType.HDAccount)
+      <div className="flex flex-col gap-7 items-center justify-center w-full">
+        {accounts && Object.keys(accounts).length > 0 ? (
+          <>
+            {Object.entries(accounts).map(([keyringAccountType, account]) => {
+              if (
+                isBitcoinBased &&
+                keyringAccountType !== KeyringAccountType.HDAccount
+              ) {
+                return null;
               }
-            >
-              <p>{account.label}</p>
 
-              <small>{ellipsis(account.address)}</small>
+              const accountList = Object.values(account);
 
-              <div
-                className={`${
-                  account.id === accountId
-                    ? 'bg-warning-success'
-                    : 'bg-brand-graylight'
-                } w-3 h-3 rounded-full border border-brand-royalblue`}
-              />
-            </li>
-          ))}
-        </ul>
-        <h2 className="mt-8">Imported Accounts</h2>
-        <ul className="scrollbar-styled flex flex-col gap-4 mt-4 px-8 w-full h-72 overflow-auto">
-          {Object.values(importedAccounts).map((account) => (
-            <li
-              className={`${
-                account.id === currentAccountId
-                  ? 'cursor-not-allowed bg-opacity-50 border-brand-royalblue'
-                  : 'cursor-pointer hover:bg-bkg-4 border-brand-royalblue'
-              } border border-solid  rounded-lg px-2 py-4 text-xs bg-bkg-2 flex justify-between items-center transition-all duration-200`}
-              key={account.id}
-              onClick={() =>
-                handleSetAccountId(account.id, KeyringAccountType.Imported)
-              }
-            >
-              <p>{account.label}</p>
+              if (!accountList.length) return null;
+              return (
+                <div
+                  key={keyringAccountType}
+                  className="h-fit flex flex-col text-center"
+                >
+                  <h3 className="text-sm font-semibold">
+                    {keyringAccountType === KeyringAccountType.HDAccount
+                      ? 'Pali Account'
+                      : keyringAccountType}
+                  </h3>
+                  <ul
+                    className={`scrollbar-styled flex flex-col gap-4 mt-4 px-8 w-full h-${
+                      accountList.length === 1 || accountList.length === 2
+                        ? 'fit'
+                        : '32'
+                    } overflow-auto`}
+                  >
+                    {accountList.map((acc) => (
+                      <li
+                        className={`${
+                          acc.id === currentAccountId &&
+                          accountType === keyringAccountType
+                            ? 'cursor-not-allowed bg-opacity-50 border-brand-royalblue'
+                            : 'cursor-pointer hover:bg-bkg-4 border-brand-royalblue'
+                        } border border-solid  rounded-lg px-2 py-4 text-xs bg-bkg-2 w-48 flex justify-between items-center transition-all duration-200`}
+                        key={`${acc.id}-${keyringAccountType}`}
+                        onClick={() => {
+                          handleSetAccountId(
+                            acc.id,
+                            keyringAccountType as KeyringAccountType
+                          );
+                        }}
+                      >
+                        <p>{acc.label}</p>
 
-              <small>{ellipsis(account.address)}</small>
+                        <div className="flex gap-3 items-center justify-center">
+                          <small>{ellipsis(acc.address)}</small>
 
-              <div
-                className={`${
-                  account.id === accountId
-                    ? 'bg-warning-success'
-                    : 'bg-brand-graylight'
-                } w-3 h-3 rounded-full border border-brand-royalblue`}
-              />
-            </li>
-          ))}
-        </ul>
+                          <div
+                            className={`${
+                              acc.id === accountId &&
+                              accountType === keyringAccountType
+                                ? 'bg-warning-success'
+                                : 'bg-brand-graylight'
+                            } w-3 h-3 rounded-full border border-brand-royalblue`}
+                          />
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <div>
+            <Icon name="loading" className="w-4 text-brand-graylight" />
+          </div>
+        )}
+
+        <small className="absolute bottom-28 text-center text-brand-royalblue text-sm">
+          Only connect with sites you trust.{' '}
+          <a href="https://docs.syscoin.org/">Learn more.</a>
+        </small>
 
         <div className="absolute bottom-10 flex items-center justify-between px-10 w-full md:max-w-2xl">
           <SecondaryButton type="button" onClick={() => window.close()}>
