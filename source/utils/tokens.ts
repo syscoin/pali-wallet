fabf0ba499f5cb9cd63c7f275116627fa26156ca
claudiovb <claudiocarvalhovilasboas@gmail.com>
2023-05-09 18:22:37 -0300

    Remove unused function

--- source/utils/tokens.ts
+++ source/utils/tokens.ts
@@ -4,9 +4,9 @@ import lodash from 'lodash';
 
 import { getErc20Abi, getErc21Abi } from '@pollum-io/sysweb3-utils';
 
+import { Queue } from 'scripts/Background/controllers/transactions/queue';
 import store from 'state/store';
 import { ITokenEthProps } from 'types/tokens';
-import { Queue } from 'scripts/Background/controllers/transactions/queue';
 const config = {
   headers: {
     'X-User-Agent':
@@ -22,57 +22,4 @@ export const getSymbolByChain = async (chain: string) => {
   );
 
   return data.symbol.toString().toUpperCase();
-};
-
-//Remove this function later, will be only in EvmAssetsController
-export const getBalanceUpdatedToErcTokens = async () => {
-  const queue = new Queue(3);
-  const { accounts, networks, activeAccount } = store.getState().vault;
-
-  const findAccount = accounts[activeAccount.type][activeAccount.id];
-  try {
-    queue.execute(async () => {
-      await Promise.all(
-        findAccount.assets.ethereum.map(async (vaultAssets: ITokenEthProps) => {
-          const provider = new ethers.providers.JsonRpcProvider(
-            networks.ethereum[vaultAssets.chainId].url
-          );
-
-          const _contract = new ethers.Contract(
-            vaultAssets.contractAddress,
-            vaultAssets.isNft ? getErc21Abi() : getErc20Abi(),
-            provider
-          );
-
-          const balanceCallMethod = await _contract.balanceOf(
-            findAccount.address
-          );
-
-          const balance = vaultAssets.isNft
-            ? Number(balanceCallMethod)
-            : `${balanceCallMethod / 10 ** Number(vaultAssets.decimals)}`;
-
-          const formattedBalance = vaultAssets.isNft
-            ? balance
-            : lodash.floor(parseFloat(balance as string), 4);
-
-          return { ...vaultAssets, balance: formattedBalance };
-        })
-      );
-    });
-
-    const results = await queue.done();
-
-    const updatedTokens = results
-      .filter((result) => result.success)
-      .map(({ result }) => result);
-
-    return updatedTokens;
-  } catch (error) {
-    console.error(
-      "Pali utils: Couldn't update assets due to the following issue ",
-      error
-    );
-    return findAccount.assets.ethereum;
-  }
 };
