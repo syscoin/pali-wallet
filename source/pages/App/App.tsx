0103f305e632fdb4413b5c1d0aba8b76d694e176
Lucas Santos <lucasgabrielgtx@gmail.com>
2023-05-16 21:14:05 -0300

    fix: solve fullscreen issue

--- source/pages/App/App.tsx
+++ source/pages/App/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { Container } from 'components/index';
 import { Router } from 'routers/index';
@@ -7,11 +7,11 @@ import { Router } from 'routers/index';
 const App: FC = () => (
   <section className="mx-auto min-w-popup h-full min-h-popup bg-bkg-2 md:max-w-2xl">
     <Container>
-      <BrowserRouter>
+      <HashRouter>
         <div className="w-full min-w-popup h-full min-h-popup">
           <Router />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </Container>
   </section>
 );
