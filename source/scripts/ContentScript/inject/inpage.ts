b34c632a1e4bcb4e1757224fd971ebc1c2196a51
Fernando Paredes Garcia <fernando@develcuy.com>
2023-05-22 23:34:04 -0500

    Warn users about UTXO network when web3 app needs window.ethereum

--- source/scripts/ContentScript/inject/inpage.ts
+++ source/scripts/ContentScript/inject/inpage.ts
@@ -15,7 +15,27 @@ const proxiedProvider = new Proxy(ethereumProvider, {
   // some common libraries, e.g. web3@1.x, mess with our API
   deleteProperty: () => true,
 });
-window.ethereum = proxiedProvider;
+
+// Handler for window.ethereum (trigger whenever window.ethereum is read)
+Object.defineProperty(window, 'ethereum', {
+  get: function() {
+    if (window.pali) {
+      if ('SCRIPT' !== window.pali.nodeName) {
+        if (window.pali.isBitcoinBased()) {
+          window.paliAlert.set('WARNING! Pali connected to UTXO network');
+          window.paliAlert.show();
+          return false;
+        } else {
+          window.paliAlert.hide();
+          return proxiedProvider; // Work as normal
+        }
+      } else {
+        // Pali is still loading...
+      }
+    }
+  }
+});
+
 shimWeb3(proxiedProvider);
 
 export const { SUPPORTED_WALLET_METHODS } = window;
--- source/scripts/ContentScript/inject/pali.ts
+++ source/scripts/ContentScript/inject/pali.ts
@@ -6,7 +6,49 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   interface Window {
     pali: Readonly<any>;
+    paliAlert: Readonly<any>;
   }
 }
 
 window.pali = new PaliInpageProviderSys();
+
+// Notification helper
+const buildPaliAlert = function() {
+  const notification = document.createElement('div');
+
+  // Style the notification
+  notification.style.position = 'fixed';
+  notification.style.top = '0';
+  notification.style.right = '0';
+  notification.style.width = '50%';
+  notification.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+  notification.style.color = '#fff';
+  notification.style.padding = '10px';
+  notification.style.fontFamily = 'Arial, sans-serif';
+  notification.style.fontSize = '12px';
+  notification.style.whiteSpace = 'nowrap';
+  notification.style.overflow = 'hidden';
+  notification.style.textOverflow = 'ellipsis';
+  notification.style.zIndex = '9999'; // Set a high z-index value
+  notification.style['pointer-events'] = 'none'; // Do not block elements below
+
+  // Append the notification to the page
+  document.body.appendChild(notification);
+
+  let methods = {
+    set: function(message) {
+      notification.innerHTML = '<p>'+message+'</p>';
+    },
+    show: function() {
+      notification.style.display = 'block';
+    },
+    hide: function() {
+      notification.style.display = 'none';
+    }
+  }
+
+  // Interactions
+  return methods;
+}
+
+window.paliAlert = buildPaliAlert();
