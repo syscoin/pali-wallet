fcfae0a44438cf1939d27128d9bf97af43402f49
leozera14 <leo_faleiros@hotmail.com>
2023-06-14 16:02:13 -0300

    chore: Trying to implement bify-module-groups

--- break-bundles-plugin.js
+++ break-bundles-plugin.js
@@ -1,44 +1,55 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
+const browserify = require('browserify');
+const browserPack = require('browser-pack');
+const { groupBySize } = require('bify-module-groups');
+const pump = require('pump');
+const fsExtra = require('fs-extra');
+const rimraf = require('rimraf');
 
 class BreakBundlesPlugin {
   apply(compiler) {
-    compiler.hooks.afterEmit.tapAsync(
+    compiler.hooks.emit.tapAsync(
       'BreakBundlesPlugin',
       (compilation, callback) => {
         const outputPath = compilation.options.output.path;
         const jsFolderPath = path.join(outputPath, 'js');
-
         const bundleFiles = fs
           .readdirSync(jsFolderPath)
           .filter((file) => file.endsWith('bundle.js'));
 
         bundleFiles.forEach((file) => {
           const mainFilePath = path.join(jsFolderPath, file);
-          const content = fs.readFileSync(mainFilePath, 'utf8');
+          const mainFileName = path.basename(file, '.bundle.js');
+          const fileSize = fs.statSync(mainFilePath).size;
 
-          if (content.length > 4 * 1024 * 1024) {
-            // 4MB in bytes
-            const numChunks = Math.ceil(content.length / (4 * 1024 * 1024));
+          if (fileSize > 4 * 1024 * 1024) {
+            const mainFileOutputDir = path.join(jsFolderPath, mainFileName);
 
-            const mainFileName = path.basename(file, '.bundle.js');
-            const subfolderPath = path.join(jsFolderPath, mainFileName);
-            fs.mkdirSync(subfolderPath);
-
-            for (let i = 0; i < numChunks; i++) {
-              const chunkContent = content.slice(
-                i * (4 * 1024 * 1024),
-                (i + 1) * (4 * 1024 * 1024)
-              );
-              const chunkFilename = `${mainFileName}_${i + 1}.bundle.js`;
-
-              fs.writeFileSync(
-                path.join(subfolderPath, chunkFilename),
-                chunkContent
-              );
+            // Remove the directory if it exists
+            if (fs.existsSync(mainFileOutputDir)) {
+              rimraf.sync(mainFileOutputDir);
             }
 
-            fs.unlinkSync(mainFilePath);
+            // Create the directory
+            fsExtra.ensureDirSync(mainFileOutputDir);
+
+            const mainFileOutputPath = path.join(mainFileOutputDir, file);
+
+            const bundler = browserify([mainFilePath]).plugin(groupBySize, {
+              sizeLimit: 4 * 1024 * 1024,
+            });
+
+            pump(
+              bundler.bundle(),
+              browserPack({ raw: true }),
+              fs.createWriteStream(mainFileOutputPath),
+              (err) => {
+                if (err) {
+                  console.error('Error splitting bundles:', err);
+                }
+              }
+            );
           }
         });
 
