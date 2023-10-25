3a61240e2f6907de6f5b479b4e5fc4623e04a7fc
Lucas Santos <lucasgabrielgtx@gmail.com>
2023-10-24 23:35:22 -0300

    feat: add verifyAddress method in ledger flow

--- source/components/Header/AccountHeader.tsx
+++ source/components/Header/AccountHeader.tsx
@@ -1,6 +1,6 @@
 import { Menu } from '@headlessui/react';
 import { toSvg } from 'jdenticon';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { browser } from 'webextension-polyfill-ts';
@@ -14,7 +14,13 @@ import importIcon from 'assets/images/import.png';
 import ledgerLogo from 'assets/images/ledgerLogo.png';
 import trezorLogo from 'assets/images/trezorLogo.png';
 import logo from 'assets/images/whiteLogo.png';
-import { IconButton, Icon } from 'components/index';
+import {
+  IconButton,
+  Icon,
+  Tooltip,
+  ConfirmationModal,
+  DefaultModal,
+} from 'components/index';
 import { useUtils } from 'hooks/index';
 import { RootState } from 'state/store';
 import { getController } from 'utils/browser';
@@ -396,11 +402,18 @@ export const AccountHeader: React.FC = () => {
   const activeAccount = useSelector(
     (state: RootState) => state.vault.activeAccount
   );
-  const { accounts } = useSelector((state: RootState) => state.vault);
+  const { accounts, isBitcoinBased, activeNetwork } = useSelector(
+    (state: RootState) => state.vault
+  );
   const { useCopyClipboard, alert, navigate } = useUtils();
   const { t } = useTranslation();
   const [copied, copy] = useCopyClipboard();
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isReconectModalOpen, setIsReconectModalOpen] = useState(false);
+  const controller = getController();
+  const isLedger = activeAccount.type === KeyringAccountType.Ledger;
+  const url = browser.runtime.getURL('app.html');
   useEffect(() => {
     const placeholder = document.querySelector('.add-identicon');
     if (!placeholder) return;
@@ -428,8 +441,53 @@ export const AccountHeader: React.FC = () => {
     alert.success(t('home.addressCopied'));
   }, [copied]);
 
+  const handleVerifyAddress = async () => {
+    try {
+      setIsLoading(true);
+      await controller.wallet.ledgerSigner.utxo.verifyUtxoAddress(
+        activeAccount.id
+      );
+      setIsLoading(false);
+      setIsOpenModal(false);
+      alert.success(t('home.addressVerified'));
+    } catch (error) {
+      const isNecessaryReconnect = error.message.includes(
+        'read properties of undefined'
+      );
+      if (isNecessaryReconnect) {
+        setIsReconectModalOpen(true);
+        return;
+      }
+      const wasDeniedByUser = error?.message?.includes('denied by the user');
+      if (wasDeniedByUser) {
+        alert.error(t('home.verificationDeniedByUser'));
+      }
+      setIsOpenModal(false);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between p-1 bg-bkg-3">
+      <ConfirmationModal
+        title={t('home.verifySysAddress')}
+        description={t('home.verifySysAddressDescription')}
+        buttonText={t('buttons.verify')}
+        onClick={handleVerifyAddress}
+        onClose={() => setIsOpenModal(false)}
+        show={isOpenModal}
+        isButtonLoading={isLoading}
+      />
+      <DefaultModal
+        show={isReconectModalOpen}
+        title={t('settings.ledgerReconnection')}
+        buttonText={t('buttons.reconnect')}
+        description={t('settings.ledgerReconnectionMessage')}
+        onClose={() => {
+          setIsReconectModalOpen(false);
+          window.open(`${url}?isReconnect=true`, '_blank');
+        }}
+      />
       <div className="flex items-center w-full text-brand-white">
         <div className="add-identicon ml-1 mr-2 my-2" />
 
@@ -450,13 +508,31 @@ export const AccountHeader: React.FC = () => {
               />
             </IconButton>
           </p>
-          <p className="text-xs">
-            {ellipsis(
-              accounts[activeAccount.type][activeAccount.id]?.address,
-              6,
-              14
-            )}
-          </p>
+          <Tooltip
+            content={
+              isLedger && isBitcoinBased && activeNetwork.chainId === 57
+                ? t('home.clickToVerify')
+                : ''
+            }
+          >
+            <p
+              className={`text-xs ${
+                isLedger && isBitcoinBased && activeNetwork.chainId === 57
+                  ? 'cursor-pointer'
+                  : ''
+              }`}
+              onClick={() => {
+                if (isLedger && isBitcoinBased && activeNetwork.chainId === 57)
+                  setIsOpenModal(true);
+              }}
+            >
+              {ellipsis(
+                accounts[activeAccount.type][activeAccount.id]?.address,
+                6,
+                14
+              )}
+            </p>
+          </Tooltip>
         </div>
 
         <IconButton
