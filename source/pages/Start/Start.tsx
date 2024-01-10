26caf3f6f009b72101044bd8fa9ee74e5314b711
GabrielPrediger <gabrielprediger046@gmail.com>
2024-01-10 09:31:09 -0300

    chore: removed component

--- source/pages/Start/Start.tsx
+++ source/pages/Start/Start.tsx
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 import { Button } from 'components/index';
 import { ImportWalletWarning } from 'components/Modal/WarningBaseModal';
 import { useUtils } from 'hooks/index';
-import { NetworkList } from 'pages/SwitchNetwork/NetworkList';
 import { RootState } from 'state/store';
 import { getController } from 'utils/browser';
 
@@ -137,7 +136,6 @@ export const Start = (props: any) => {
           Wallet
         </h1>
       </div>
-      <NetworkList isChanging={null} />
 
       {isFirstStep ? getStarted : unLock}
     </div>
