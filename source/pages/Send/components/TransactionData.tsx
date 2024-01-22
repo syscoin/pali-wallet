6ccb6424357452da8dda48ba33238d7f8b202c14
GabrielPrediger <gabrielprediger046@gmail.com>
2024-01-22 19:18:46 -0300

    feat(SendTransactions): tx data tab

--- source/pages/Send/components/TransactionData.tsx
+++ source/pages/Send/components/TransactionData.tsx
@@ -11,13 +11,7 @@ export const TransactionDataComponent = (props: ITransactionDataProps) => {
   const { decodedTx } = props;
   const { t } = useTranslation();
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <p className="flex gap-1.5 items-center justify-start w-full text-sm">
-        {t('send.method')}:
-        <span className="text-brand-royalblue font-bold">
-          {decodedTx.method}
-        </span>
-      </p>
+    <div className="flex p-6 rounded-[20px] flex-col items-center justify-center bg-brand-blue600 w-[400px] h-[420px] relative left-[-1%]">
       {decodedTx.method === 'Contract Interaction' ? (
         <div
           className="flex items-center justify-center mt-2 p-4 w-full text-xs rounded-xl"
@@ -26,10 +20,7 @@ export const TransactionDataComponent = (props: ITransactionDataProps) => {
           <p>{t('send.decodificationNotAvailable')}</p>
         </div>
       ) : (
-        <pre
-          className="scrollbar-styled mb-6 mt-2 px-2.5 py-1 w-full max-h-80 text-xs rounded-xl overflow-y-scroll"
-          style={{ backgroundColor: 'rgba(22, 39, 66, 1)' }}
-        >
+        <pre className="scrollbar-styled mb-6 mt-2 px-2.5 py-1  max-h-80 text-xs rounded-xl overflow-y-scroll whitespace-pre-wrap">
           {JSON.stringify(decodedTx, null, '\t')}
         </pre>
       )}
