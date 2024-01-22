116f7f5ea48cd82e24050f0b73dcb47bfb9c1043
GabrielPrediger <gabrielprediger046@gmail.com>
2024-01-22 11:30:54 -0300

    fix: timestamp fn

--- source/pages/Home/Panel/components/Transactions/utils/useTransactionsInfos.tsx
+++ source/pages/Home/Panel/components/Transactions/utils/useTransactionsInfos.tsx
@@ -137,13 +137,15 @@ export const useTransactionsListConfig = (
   };
 
   const formatTimeStampUtxo = (timestamp: number) => {
-    const date = new Date(timestamp * 1000);
+    const date = new Date(timestamp);
+    console.log(timestamp, 'a');
 
     const dateFormatOptions: Intl.DateTimeFormatOptions = {
       month: '2-digit',
       day: '2-digit',
       year: '2-digit',
     };
+    console.log(dateFormatOptions, 'b');
 
     const timeFormatOptions: Intl.DateTimeFormatOptions = {
       hour: 'numeric',
@@ -159,9 +161,11 @@ export const useTransactionsListConfig = (
       'en-US',
       timeFormatOptions
     ).format(date);
+    console.log(formattedDate, 'c');
 
     const dateComponents = formattedDate.split('/');
     const formattedDateWithHyphen = dateComponents.join('-');
+    console.log(formattedDateWithHyphen, 'd');
 
     return (
       <div className="flex gap-2">
