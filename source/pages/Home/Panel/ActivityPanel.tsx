8a47524defe8a96c2609c40184bafc58973acb4d
GabrielPrediger <gabrielprediger046@gmail.com>
2024-02-29 11:52:41 -0300

    fix(EvmList): code improvment

--- source/pages/Home/Panel/ActivityPanel.tsx
+++ source/pages/Home/Panel/ActivityPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
