71b07090c8ec101e683792e8990e31a9bf0db659
GabrielPrediger <gabrielprediger046@gmail.com>
2024-03-01 15:36:30 -0300

    fix(ActivityPanel): hasTransaction crash error

--- source/pages/Home/Panel/ActivityPanel.tsx
+++ source/pages/Home/Panel/ActivityPanel.tsx
@@ -54,7 +54,7 @@ export const TransactionsPanel = () => {
 
   const hasTransactions = useMemo(
     () => (hasTransactions ? transactions : previousTransactions),
-    []
+    [transactions, previousTransactions]
   );
 
   useEffect(() => {
