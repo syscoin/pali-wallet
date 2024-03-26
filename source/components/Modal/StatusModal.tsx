ef68f7be125fdddbcb79719c50b198f97c62ce7b
Gabriel Prediger <gabrielprediger@gmail.com>
2024-03-26 16:37:29 -0300

    style(WalletPhrase): new style

--- source/components/Modal/StatusModal.tsx
+++ source/components/Modal/StatusModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment, useCallback, useMemo } from 'react';
+import React, { Fragment, useCallback, useEffect, useMemo } from 'react';
 
 interface IModal {
   description?: string;
