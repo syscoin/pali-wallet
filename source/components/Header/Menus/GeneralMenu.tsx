745121260d40dc98d65d3e1c86b3ae64b26745a3
Gabriel Prediger <gabrielprediger@gmail.com>
2024-03-26 10:14:18 -0300

    style(GeneralMenu): changed style

--- source/components/Header/Menus/GeneralMenu.tsx
+++ source/components/Header/Menus/GeneralMenu.tsx
@@ -116,7 +116,7 @@ export const GeneralMenu: React.FC = () => {
 
         <Menu.Items
           as="div"
-          className="scrollbar-styled absolute z-50 right-0 pb-6 w-72 h-100 text-center text-brand-white font-poppins bg-brand-blue600 rounded-2xl focus:outline-none shadow-2xl overflow-auto origin-top-right ring-1 ring-black ring-opacity-5"
+          className="scrollbar-styled absolute z-50 top-6 right-[-16px] pb-24 w-screen h-screen text-center text-brand-white font-poppins bg-brand-blue600 rounded-2xl focus:outline-none shadow-2xl overflow-auto  ring-1 ring-black ring-opacity-5"
         >
           <AccountMenu />
 
