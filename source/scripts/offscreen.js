edd97d577dd4df869b3d3653192b11b30e765708
jagdeep sidhu <sidhujag@syscoin.org>
2025-07-22 10:44:54 -0700

    fix CSP warnings in offscreen doc

---
+++ source/scripts/offscreen.js
@@ -0,0 +1,5 @@
+// Set flag to prevent app initialization in offscreen context
+window.__PALI_OFFSCREEN__ = true;
+
+// Log that offscreen document is ready
+console.log('[Offscreen] Document initialized');
